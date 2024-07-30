a7cca311b126de0a3da34a1124ce184e91f9b862
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-30 13:25:05 +0400

    Add jwt token model

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Models/TokenModels/TokenModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RegisterMicroservice.Api.Models.TokenModels
+{
+    public class TokenModel
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
