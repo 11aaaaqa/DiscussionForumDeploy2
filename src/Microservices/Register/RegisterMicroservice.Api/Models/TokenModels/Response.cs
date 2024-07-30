30d3127777d678505ad93990beed0221911f7e27
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-30 13:24:58 +0400

    Add response model

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Models/TokenModels/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace RegisterMicroservice.Api.Models.TokenModels
+{
+    public class Response
+    {
+        public string Message { get; set; }
+        public string Status { get; set; }
+    }
+}
