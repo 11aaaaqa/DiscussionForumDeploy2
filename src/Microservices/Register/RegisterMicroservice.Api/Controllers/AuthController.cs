4fd75c783810e0c53ff17f85bed6a3570309b70a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-30 14:59:46 +0400

    Implement register method

--- src/Microservices/Register/RegisterMicroservice.Api/Controllers/AuthController.cs
+++ src/Microservices/Register/RegisterMicroservice.Api/Controllers/AuthController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using RegisterMicroservice.Api.DTOs;
+using RegisterMicroservice.Api.Models.TokenModels;
+using RegisterMicroservice.Api.Models.UserModels;
 
 namespace RegisterMicroservice.Api.Controllers
 {
@@ -7,5 +12,57 @@ namespace RegisterMicroservice.Api.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<User> userManager;
+        private readonly ILogger<AuthController> logger;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration configuration;
+
+        public AuthController(UserManager<User> userManager, ILogger<AuthController> logger, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.logger = logger;
+            this.roleManager = roleManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            var checkUser = await userManager.FindByEmailAsync(model.Email);
+            if (checkUser != null)
+            {
+                logger.LogInformation("User wasn't created due to \"User already exists\"");
+                return BadRequest(new Response{Message = "User already exists", Status = "Error"});
+            }
+
+            var user = new User
+            {
+                Answers = 0,
+                Id = Guid.NewGuid(),
+                Email = model.Email,
+                EmailConfirmed = false,
+                Posts = 0,
+                RegisteredAt = DateOnly.FromDateTime(DateTime.Now),
+                UserName = model.UserName
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                logger.LogInformation("User wasn't created due to \"Something went wrong\"");
+                return BadRequest(new Response { Status = "Error", Message = "Something went wrong" });
+            }
+                
+            logger.LogInformation("New user was created");
+            return Ok(new Response { Status = "Success", Message = "User was successfully created" });
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginDto model)
+        {
+            return Ok();
+        }
     }
 }
