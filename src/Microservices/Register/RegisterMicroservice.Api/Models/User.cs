5a2cb6de8e7c68948fb3c72776122313a1643cc2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-30 12:52:39 +0400

    Add user model

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RegisterMicroservice.Api.Models
+{
+    public class User : IdentityUser
+    {
+        public new Guid Id { get; set; }
+        public uint Posts { get; set; }
+        public uint Answers { get; set; }
+        public DateOnly RegisteredAt { get; set; }
+    }
+}
