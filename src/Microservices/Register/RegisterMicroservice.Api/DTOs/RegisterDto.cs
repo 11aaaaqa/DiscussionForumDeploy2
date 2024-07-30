0a6c881009efdd69a3692aafab888715b4fadf63
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-30 13:21:33 +0400

    Add register user dto

---
+++ src/Microservices/Register/RegisterMicroservice.Api/DTOs/RegisterDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegisterMicroservice.Api.DTOs
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "Поле \"Эл. почта\" обязательно")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Эл. почта")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Имя пользователя\" обязательно")]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Пароль\" обязательно")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Подтвердить пароль\" обязательно")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
+
