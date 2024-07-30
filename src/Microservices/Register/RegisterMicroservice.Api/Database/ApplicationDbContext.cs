32e6c4496c5d795fd5728e8138d6f4545aff5c5e
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-30 12:56:41 +0400

    Add database context

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RegisterMicroservice.Api.Models;
+
+namespace RegisterMicroservice.Api.Database
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .Ignore(x => x.PhoneNumber)
+                .Ignore(x => x.PhoneNumberConfirmed)
+                .Ignore(x => x.TwoFactorEnabled)
+                .Ignore(x => x.LockoutEnabled)
+                .Ignore(x => x.LockoutEnd)
+                .Ignore(x => x.AccessFailedCount)
+                .ToTable("Users");
+        }
+    }
+}
