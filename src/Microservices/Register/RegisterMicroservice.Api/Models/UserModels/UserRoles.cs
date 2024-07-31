7db0093028fcf273c728fbff2134d792d261d85f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-31 11:15:37 +0400

    Remove user roles class

--- src/Microservices/Register/RegisterMicroservice.Api/Models/UserModels/UserRoles.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RegisterMicroservice.Api.Models.UserModels
-{
-    public static class UserRoles
-    {
-        public const string Admin = "Admin";
-        public const string User = "User";
-    }
-}
