09e9b69e2c5c4a8fdfe0d640419b677ad42f5863
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-01 16:54:01 +0400

    Added register response model

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Models/Response/RegisterResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RegisterMicroservice.Api.Models.Response
+{
+    public class RegisterResponseModel
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
