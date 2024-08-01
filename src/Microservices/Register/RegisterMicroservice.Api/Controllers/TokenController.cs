4ff3de22624757da39840b63cf8aea0e2941a899
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-01 11:47:51 +0400

    Reapply "Update jwt bearer setting added clock skew to 0 seconds"
    This reverts commit 38e67e202e3b7d7e9590891d0a8c1ab66430326a.

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Controllers/TokenController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RegisterMicroservice.Api.Models.Jwt;
+using RegisterMicroservice.Api.Models.UserModels;
+using RegisterMicroservice.Api.Services;
+
+namespace RegisterMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly UserManager<User> userManager;
+        private readonly ITokenService tokenService;
+        private readonly ILogger<TokenController> logger;
+
+        public TokenController(UserManager<User> userManager, ITokenService tokenService, ILogger<TokenController> logger)
+        {
+            this.userManager = userManager;
+            this.tokenService = tokenService;
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        [Route("refresh")]
+        public async Task<IActionResult> Refresh(TokenApiModel tokenModel)
+        {
+            if (tokenModel is null)
+            {
+                return BadRequest("Invalid request");
+            }
+
+            string? accessToken = tokenModel.AccessToken;
+            string? refreshToken = tokenModel.RefreshToken;
+
+            var principal = tokenService.GetPrincipalFromExpiredToken(accessToken);
+            var userName = principal.Identity.Name;
+
+            var user = await userManager.FindByNameAsync(userName);
+
+            if (user == null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
+            {
+                return BadRequest("Invalid request");
+            }
+
+            var newAccessToken = tokenService.GenerateAccessToken(principal.Claims);
+            var newRefreshToken = tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = newRefreshToken;
+            await userManager.UpdateAsync(user);
+            return Ok(new AuthenticatedResponse
+            {
+                RefreshToken = newRefreshToken,
+                Token = newAccessToken
+            });
+        }
+
+        [HttpPost]
+        [Route("revoke")]
+        public async Task<IActionResult> Revoke()
+        {
+            var userName = User.Identity.Name;
+
+            var user = await userManager.FindByNameAsync(userName);
+            if (user is null)
+            {
+                return BadRequest("Invalid request");
+            }
+
+            user.RefreshToken = null;
+            await userManager.UpdateAsync(user);
+            return Ok();
+        }
+    }
+}
