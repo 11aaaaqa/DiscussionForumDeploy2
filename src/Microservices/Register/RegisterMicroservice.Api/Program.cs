cd7ddcac049e6be51aeb36614ac1b5dbb81b9566
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-01 14:42:12 +0400

    Add password validation settings

--- src/Microservices/Register/RegisterMicroservice.Api/Program.cs
+++ src/Microservices/Register/RegisterMicroservice.Api/Program.cs
@@ -10,7 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseNpgsql(
     builder.Configuration["ConnectionStrings:DefaultConnection"]));
-builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<User, IdentityRole>(x =>
+{
+    x.Password.RequireLowercase = false;
+    x.Password.RequireUppercase = false;
+    x.Password.RequireDigit = false;
+    x.Password.RequireNonAlphanumeric = false;
+    x.Password.RequiredLength = 8;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
