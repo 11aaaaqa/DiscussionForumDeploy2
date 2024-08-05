bd5318655f7d0f13035ea7f93db90969eeb1ce78
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-05 15:50:01 +0400

    Add method sends email with password recovery link

--- src/Microservices/Register/RegisterMicroservice.Api/Controllers/UserController.cs
+++ src/Microservices/Register/RegisterMicroservice.Api/Controllers/UserController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using RegisterMicroservice.Api.DTOs.ResetPassword;
+using RegisterMicroservice.Api.Models.UserModels;
+using RegisterMicroservice.Api.Services;
 
 namespace RegisterMicroservice.Api.Controllers
 {
@@ -7,5 +12,40 @@ namespace RegisterMicroservice.Api.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserManager<User> userManager;
+        private readonly ILogger<AuthController> logger;
+        private readonly IConfiguration configuration;
+        private readonly IEmailSender emailSender;
+
+        public UserController(UserManager<User> userManager, ILogger<AuthController> logger, IConfiguration configuration, IEmailSender emailSender)
+        {
+            this.userManager = userManager;
+            this.logger = logger;
+            this.configuration = configuration;
+            this.emailSender = emailSender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordDto model, string callbackController, string callbackMethod)
+        {
+            var user = await userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return BadRequest("User with current email doesn't exist");
+            }
+
+            if (!await userManager.IsEmailConfirmedAsync(user))
+            {
+                return BadRequest("Email doesn't confirmed");
+            }
+
+            var token = await userManager.GeneratePasswordResetTokenAsync(user);
+            var callbackUrl = Url.Action(callbackMethod, callbackController, new {userId = user.Id, token = token},
+                protocol: HttpContext.Request.Scheme);
+            await emailSender.SendEmailAsync(new MailboxAddress("", model.Email), "Сброс пароля",
+                $"Для сброса пароля перейдите по <a href=\"{callbackUrl}\">ссылке</a>");
+
+            return Content("Для сброса пароля проверьте электронную почту и перейдите по ссылке, указанной в письме");
+        }
     }
 }
