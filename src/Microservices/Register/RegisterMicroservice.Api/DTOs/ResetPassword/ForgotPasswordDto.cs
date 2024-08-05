528bb76fcca4f9683a3a90edc853b69c05524f2b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-05 15:30:29 +0400

    Add DTOs for password recovery

---
+++ src/Microservices/Register/RegisterMicroservice.Api/DTOs/ResetPassword/ForgotPasswordDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegisterMicroservice.Api.DTOs.ResetPassword
+{
+    public class ForgotPasswordDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
