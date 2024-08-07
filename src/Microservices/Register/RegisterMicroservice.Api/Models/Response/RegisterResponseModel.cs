333b7c61a15791b735df8afcb9fdd0e12b24b9c0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-07 14:31:57 +0400

    Remove unused classes

--- src/Microservices/Register/RegisterMicroservice.Api/Models/Response/RegisterResponseModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RegisterMicroservice.Api.Models.Response
-{
-    public class RegisterResponseModel
-    {
-        public string Status { get; set; }
-        public string Message { get; set; }
-    }
-}
