77f562b7a1bf3127b2d0a713681968fb69b45b65
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-21 11:16:37 +0400

    Move to apiResponses to GeneralsClassesLib

--- src/Microservices/Register/RegisterMicroservice.Api/Models/Jwt/AuthenticatedResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RegisterMicroservice.Api.Models.Jwt
-{
-    public class AuthenticatedResponse
-    {
-        public string? Token { get; set; }
-        public string? RefreshToken { get; set;}
-    }
-}
