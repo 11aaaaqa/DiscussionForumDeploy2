1452514dab81a900cf2ec8ea60c5a3ee9d794143
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-23 11:06:51 +0400

    Add databse context

---
+++ src/Microservices/User/UserMicroservie.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Models;
+
+namespace UserMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
