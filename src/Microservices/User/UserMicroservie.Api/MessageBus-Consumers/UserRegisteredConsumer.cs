658548cb1f3958961578e5dbd07c941e3f63048d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-23 15:27:39 +0400

    Add message consumer

---
+++ src/Microservices/User/UserMicroservie.Api/MessageBus-Consumers/UserRegisteredConsumer.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using UserMicroservice.Api.Database;
+using UserMicroservice.Api.Models;
+
+namespace UserMicroservice.Api.MessageBus_Consumers
+{
+    public class UserRegisteredConsumer : IConsumer<IUserRegistered>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserRegisteredConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserRegistered> context)
+        {
+            await databaseContext.Users.AddAsync(new User
+            {
+                Id = Guid.NewGuid(),
+                UserName = context.Message.UserName,
+                RegisteredAt = DateOnly.FromDateTime(DateTime.Now),
+                Posts = 0,
+                Answers = 0,
+                AspNetUserId = context.Message.UserId
+            });
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
