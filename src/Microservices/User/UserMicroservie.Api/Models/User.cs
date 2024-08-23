43eb7c8df98512d3f3a3af89db65581d3f8c8a12
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-23 11:06:33 +0400

    Add user model

---
+++ src/Microservices/User/UserMicroservie.Api/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserMicroservice.Api.Models
+{
+    [Index(nameof(UserName), IsUnique = true)]
+    public class User
+    {
+        public string AspNetUserId { get; set; }
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public uint Posts { get; set; }
+        public uint Answers { get; set; }
+        public DateOnly RegisteredAt { get; set; }
+    }
+}
