26a800bfa9c65e09b6f5770aab2e990192cc10c5
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-23 15:37:19 +0400

    Remove properties

--- src/Microservices/Register/RegisterMicroservice.Api/Models/UserModels/User.cs
+++ src/Microservices/Register/RegisterMicroservice.Api/Models/UserModels/User.cs
@@ -4,9 +4,6 @@ namespace RegisterMicroservice.Api.Models.UserModels
 {
     public class User : IdentityUser
     {
-        public uint Posts { get; set; }
-        public uint Answers { get; set; }
-        public DateOnly RegisteredAt { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }
     }
