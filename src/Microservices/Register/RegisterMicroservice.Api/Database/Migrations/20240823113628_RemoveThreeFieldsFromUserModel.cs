7a3897aa4e791c0000cef7b7d122c679f1da9a55
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-23 15:37:53 +0400

    Add migration

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Database/Migrations/20240823113628_RemoveThreeFieldsFromUserModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegisterMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveThreeFieldsFromUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Answers",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Posts",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RegisteredAt",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Answers",
+                table: "Users",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Posts",
+                table: "Users",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "RegisteredAt",
+                table: "Users",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+    }
+}
