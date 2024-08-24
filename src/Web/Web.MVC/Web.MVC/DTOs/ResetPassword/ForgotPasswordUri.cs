1e588ae0ab4f45ec73c84dc20b3ffe7afb53e042
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-24 21:16:04 +0400

    Add uri model

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/ResetPassword/ForgotPasswordUri.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.MVC.DTOs.ResetPassword
+{
+    public class ForgotPasswordUri
+    {
+        public string Protocol { get; set; }
+        public string DomainName { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
