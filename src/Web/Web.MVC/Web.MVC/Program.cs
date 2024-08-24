f75809c939b7568858e5e91459bdd1e2b51fcb43
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-24 12:56:20 +0400

    Add jwt bearer

--- src/Web/Web.MVC/Web.MVC/Program.cs
+++ src/Web/Web.MVC/Web.MVC/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.SaveToken = true;
+    x.RequireHttpsMetadata = false;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]))
+    };
+});
 
 builder.Services.AddHttpClient();
 
@@ -17,6 +40,16 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    context.Request.Cookies.TryGetValue("accessToken", out var accessToken);
+    if (accessToken != null)
+    {
+        context.Request.Headers.Add("Authorization", "Bearer " + accessToken);
+    }
+    await next();
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
