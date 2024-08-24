9165768c86064d86e17dd4b003dee00414a3f9cd
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-24 21:16:14 +0400

    Add dto

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/ResetPassword/ForgotPasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.ResetPassword
+{
+    public class ForgotPasswordDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public ForgotPasswordUri? Uri { get; set; }
+    }
+}
