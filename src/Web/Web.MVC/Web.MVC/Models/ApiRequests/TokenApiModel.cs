bcc5ca99d0aa7de91df3b9fbd530c90a9735fcca
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-24 18:53:15 +0400

    Add model for request

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiRequests/TokenApiModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Models.ApiRequests
+{
+    public class TokenApiModel
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
