b9f1850fe50a4ab265d3082f2ab5563106e7c2c3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-24 18:53:47 +0400

    Add model for api response

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/GetUserResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace RegisterMicroservice.Api.Models.ApiResponses
+{
+    public class GetUserResponse
+    {
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+        public string Id { get; set; }
+        public bool EmailConfirmed { get; set; }
+    }
+}
