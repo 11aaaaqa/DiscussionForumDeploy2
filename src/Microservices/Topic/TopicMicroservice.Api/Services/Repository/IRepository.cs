75306f98c9edde22fa085db38bc999d25fe6a9ca
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-26 10:54:02 +0400

    Add repository interface

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Services/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace TopicMicroservice.Api.Services.Repository
+{
+    public interface IRepository<TModel>
+    {
+        Task<List<TModel>> GetAllAsync();
+        Task<TModel?> GetByIdAsync(Guid id);
+        Task<TModel?> GetByNameAsync(string name);
+        Task<TModel> UpdateAsync(TModel model);
+        Task<TModel> CreateAsync(TModel model);
+        Task DeleteByNameAsync(string name);
+        Task DeleteByIdAsync(Guid id);
+    }
+}
