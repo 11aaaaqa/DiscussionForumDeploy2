59252820ca76c6a5a51602e4643585bb3113e648
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-26 18:31:43 +0400

    Add IDiscussion added event consumer

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Services/MessageBus-Consumers/DiscussionAddedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using TopicMicroservice.Api.Models;
+using TopicMicroservice.Api.Services.Repository;
+
+namespace TopicMicroservice.Api.Services.MessageBus_Consumers
+{
+    public class DiscussionAddedConsumer : IConsumer<IDiscussionAdded>
+    {
+        private readonly IRepository<Topic> topicRepository;
+
+        public DiscussionAddedConsumer(IRepository<Topic> topicRepository)
+        {
+            this.topicRepository = topicRepository;
+        }
+        public async Task Consume(ConsumeContext<IDiscussionAdded> context)
+        {
+            var topic = await topicRepository.GetByNameAsync(context.Message.TopicName);
+            topic.PostsCount += context.Message.DiscussionCount;
+            await topicRepository.UpdateAsync(topic);
+        }
+    }
+}
