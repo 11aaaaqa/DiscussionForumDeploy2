b1b807b99819afbeb69a08403e472b1076075fb1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-26 13:32:00 +0400

    Add discussion model

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Models/Discussion.cs
@@ -0,0 +1,13 @@
+﻿namespace DiscussionMicroservice.Api.Models
+{
+    public class Discussion
+    {
+        public Guid Id { get; set; }
+        public string TopicName { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Rating { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
