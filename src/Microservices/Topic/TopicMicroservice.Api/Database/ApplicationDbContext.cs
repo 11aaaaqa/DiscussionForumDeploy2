66c81d36b2614457712bf0bdf851a5313a662c9a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-26 10:27:58 +0400

    Add database context

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopicMicroservice.Api.Models;
+
+namespace TopicMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
