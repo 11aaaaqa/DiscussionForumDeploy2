f64eaa13f3037682ad7ce46f20af67eb786898e9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-26 10:54:15 +0400

    Add topic repository

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Services/Repository/TopicRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopicMicroservice.Api.Database;
+using TopicMicroservice.Api.Models;
+
+namespace TopicMicroservice.Api.Services.Repository
+{
+    public class TopicRepository : IRepository<Topic>
+    {
+        private readonly ApplicationDbContext context;
+
+        public TopicRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Topic>> GetAllAsync() => await context.Topics.ToListAsync();
+
+        public async Task<Topic?> GetByIdAsync(Guid id) => await context.Topics.SingleOrDefaultAsync(x => x.Id == id);
+
+
+        public async Task<Topic?> GetByNameAsync(string name) => await context.Topics.SingleOrDefaultAsync(x => x.Name == name);
+
+        public async Task<Topic> UpdateAsync(Topic model)
+        {
+            context.Topics.Update(model);
+            await context.SaveChangesAsync();
+            return model;
+        }
+
+        public async Task<Topic> CreateAsync(Topic model)
+        {
+           await context.Topics.AddAsync(model);
+           await context.SaveChangesAsync();
+           return model;
+        }
+
+        public async Task DeleteByNameAsync(string name)
+        {
+            context.Topics.Remove(new Topic { Name = name });
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(Guid id)
+        {
+            context.Topics.Remove(new Topic { Id = id });
+            await context.SaveChangesAsync();
+        }
+    }
+}
