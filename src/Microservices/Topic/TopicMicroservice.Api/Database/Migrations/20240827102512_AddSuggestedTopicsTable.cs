64879e5423df3cf43cd07b278a20a79d1d774b29
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-27 14:26:17 +0400

    Add migration

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Database/Migrations/20240827102512_AddSuggestedTopicsTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TopicMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSuggestedTopicsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Topics",
+                table: "Topics");
+
+            migrationBuilder.RenameTable(
+                name: "Topics",
+                newName: "Topic");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Topics_Name",
+                table: "Topic",
+                newName: "IX_Topic_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Topic",
+                table: "Topic",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Topic",
+                table: "Topic");
+
+            migrationBuilder.RenameTable(
+                name: "Topic",
+                newName: "Topics");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Topic_Name",
+                table: "Topics",
+                newName: "IX_Topics_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Topics",
+                table: "Topics",
+                column: "Id");
+        }
+    }
+}
