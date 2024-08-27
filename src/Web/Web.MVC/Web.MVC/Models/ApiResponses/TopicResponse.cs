f1bc86a45df58f868874a977d1041771a6f9b31a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-27 17:10:14 +0400

    Add topic response model

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/TopicResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.MVC.Models.ApiResponses
+{
+    public class TopicResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public uint PostsCount { get; set; }
+    }
+}
