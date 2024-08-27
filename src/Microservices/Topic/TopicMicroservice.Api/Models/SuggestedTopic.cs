777170668ddb0341c9350a0b48c0259cebb9d323
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-27 17:09:08 +0400

    Add suggested topic model

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Models/SuggestedTopic.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TopicMicroservice.Api.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class SuggestedTopic
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public uint PostsCount { get; set; }
+    }
+}
