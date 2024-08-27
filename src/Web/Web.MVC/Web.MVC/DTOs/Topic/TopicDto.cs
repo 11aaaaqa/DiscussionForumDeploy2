8e5189dfacd160d69e69ba862a2ef2af9b782e9d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-27 17:09:49 +0400

    Add topic dto

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/Topic/TopicDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Topic
+{
+    public class TopicDto
+    {
+        [Required]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+    }
+}
