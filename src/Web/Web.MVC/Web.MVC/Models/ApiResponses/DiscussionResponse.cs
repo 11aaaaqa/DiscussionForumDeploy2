e2b7d0d61235c4f637487990ce6021231d3cf29b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-28 16:49:20 +0400

    Add discussion response model

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/DiscussionResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.MVC.Models.ApiResponses
+{
+    public class DiscussionResponse
+    {
+        public Guid Id { get; set; }
+        public string TopicName { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Rating { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
