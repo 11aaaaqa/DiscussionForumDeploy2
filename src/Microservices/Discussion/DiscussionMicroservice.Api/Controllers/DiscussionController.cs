8b8cf45691ae80d0b59f2937f46217f85af082f2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-28 16:48:42 +0400

    Add controller

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Controllers/DiscussionController.cs
@@ -0,0 +1,23 @@
+﻿using DiscussionMicroservice.Api.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscussionMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscussionController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public DiscussionController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [Route("GetDiscussionsByTopicName")]
+        [HttpGet]
+        public async Task<IActionResult> GetDiscussionsByTopicNameAsync(string topicName) =>
+            Ok(await context.Discussions.Where(x => x.TopicName == topicName).ToListAsync());
+    }
+}
