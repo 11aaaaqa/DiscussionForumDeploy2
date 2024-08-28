13d722d1573738889bdcb092cdd8793ada27a63b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-28 17:10:46 +0400

    Add migration

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Database/Migrations/20240828130956_AddSuggestedDiscussionsTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscussionMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSuggestedDiscussionsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Discussion",
+                table: "Discussion");
+
+            migrationBuilder.RenameTable(
+                name: "Discussion",
+                newName: "Discussions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Discussions",
+                table: "Discussions",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "SuggestedDiscussions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TopicName = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuggestedDiscussions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuggestedDiscussions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Discussions",
+                table: "Discussions");
+
+            migrationBuilder.RenameTable(
+                name: "Discussions",
+                newName: "Discussion");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Discussion",
+                table: "Discussion",
+                column: "Id");
+        }
+    }
+}
