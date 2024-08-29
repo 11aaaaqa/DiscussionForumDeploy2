7d640c2b3ee5f46d03d88a8b8846c20b48d4ff00
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-29 12:18:36 +0400

    Add migration

--- src/Microservices/Discussion/DiscussionMicroservice.Api/Database/Migrations/20240829075629_MakeCreationDateDateOnlyStructureInsteadOfDateTime.Designer.cs
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Database/Migrations/20240829075629_MakeCreationDateDateOnlyStructureInsteadOfDateTime.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DiscussionMicroservice.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DiscussionMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240829075629_MakeCreationDateDateOnlyStructureInsteadOfDateTime")]
+    partial class MakeCreationDateDateOnlyStructureInsteadOfDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
