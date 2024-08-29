91834801385d924b3311c83d68814eafe067b4dd
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-29 12:18:29 +0400

    Make created at DateOnly structure instead of DateTime

--- src/Microservices/Discussion/DiscussionMicroservice.Api/Models/Discussion.cs
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Models/Discussion.cs
@@ -6,7 +6,7 @@
         public string TopicName { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateOnly CreatedAt { get; set; }
         public int Rating { get; set; }
         public string CreatedBy { get; set; }
     }
