16ff7b6fb720cb0c129c930a72902fbb363a7710
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-29 12:20:09 +0400

    Add dto

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/Discussion/DiscussionDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Discussion
+{
+    public class DiscussionDto
+    {
+        public string TopicName { get; set; }
+
+        [Required]
+        [Display(Name = "Заголовок")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Обсуждение")]
+        public string Content { get; set; }
+    }
+}
