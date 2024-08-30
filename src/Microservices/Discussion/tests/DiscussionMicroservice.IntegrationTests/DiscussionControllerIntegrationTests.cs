df73929cfb0bb765558734c63ac991773d1a9ade
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-30 18:39:05 +0400

    Add Integration test

---
+++ src/Microservices/Discussion/tests/DiscussionMicroservice.IntegrationTests/DiscussionControllerIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using DiscussionMicroservice.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscussionMicroservice.IntegrationTests
+{
+    public class DiscussionControllerIntegrationTests : IClassFixture<TestingWebAppFactory<Program>>
+    {
+        private readonly HttpClient client;
+
+        public DiscussionControllerIntegrationTests(TestingWebAppFactory<Program> factory)
+        {
+            client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetDiscussionByIdAsync_ReturnsBadRequest()
+        {
+            var response = await client.GetAsync($"/api/Discussion/GetDiscussionById?id={Guid.Empty}");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
