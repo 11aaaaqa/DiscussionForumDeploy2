f10a781fb51a3280c3e1cea25216671d54e8f98c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-31 12:59:10 +0400

    Move SuggestDiscussionController tests

--- src/Microservices/Discussion/tests/DiscussionMicroservice.IntegrationTests/DiscussionControllerIntegrationTests.cs
+++ src/Microservices/Discussion/tests/DiscussionMicroservice.IntegrationTests/DiscussionControllerIntegrationTests.cs
@@ -1,5 +1,8 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using DiscussionMicroservice.Api.DTOs;
 using DiscussionMicroservice.Api.Models;
 
 namespace DiscussionMicroservice.IntegrationTests
@@ -44,6 +47,66 @@ namespace DiscussionMicroservice.IntegrationTests
             Assert.Equal(2, discussions.Count);
             Assert.Equal(topicName, discussions[0].TopicName);
             Assert.Equal(topicName, discussions[1].TopicName);
+        }
+
+        [Fact]
+        public async Task GetSuggestedDiscussionByIdAsync_ReturnsBadRequest()
+        {
+            var response = await client.GetAsync($"api/SuggestDiscussion/GetSuggestedDiscussionById?id={Guid.Empty}");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetSuggestedDiscussionByIdAsync_ReturnsOkWithSuggestedDiscussion()
+        {
+            var id = new Guid("1fd24884-7dd7-40a9-bfe5-f8247a5e3bc7");
+            var response = await client.GetAsync($"api/SuggestDiscussion/GetSuggestedDiscussionById?id={id}");
+
+            response.EnsureSuccessStatusCode();
+            var suggestedDiscussion = await response.Content.ReadFromJsonAsync<SuggestedDiscussion>();
+            Assert.NotNull(suggestedDiscussion);
+            Assert.Equal(id, suggestedDiscussion.Id);
+        }
+
+        [Fact]
+        public async Task GetAllSuggestedDiscussionsAsync_ReturnOkWithListOfSuggestedDiscussions()
+        {
+            var response = await client.GetAsync("api/SuggestDiscussion/GetAllSuggestedDiscussions");
+
+            response.EnsureSuccessStatusCode();
+            var suggestedDiscussions = await response.Content.ReadFromJsonAsync<List<SuggestedDiscussion>>();
+            Assert.NotNull(suggestedDiscussions);
+            Assert.Equal(2, suggestedDiscussions.Count);
+        }
+
+        [Fact]
+        public async Task SuggestToCreateDiscussionAsync_ReturnsOk()
+        {
+            var model = new DiscussionDto { Content = "TestContent1212", CreatedBy = "TestCreatedBy1212", Title = "TestTitle1212" };
+            using StringContent content = new(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("api/SuggestDiscussion/SuggestToCreate?topicName=TestTopicName1212", content);
+
+            response.EnsureSuccessStatusCode();
+            var resp = await client.GetAsync("api/SuggestDiscussion/GetAllSuggestedDiscussions");
+            resp.EnsureSuccessStatusCode();
+            var suggestedDiscussions = await resp.Content.ReadFromJsonAsync<List<SuggestedDiscussion>>();
+            var addedSuggestedDiscussion = suggestedDiscussions.Where(x => x.Content == "TestContent1212")
+                .Where(x => x.CreatedBy == "TestCreatedBy1212").Where(x => x.Title == "TestTitle1212")
+                .SingleOrDefault();
+            Assert.NotNull(addedSuggestedDiscussion);
+        }
+
+        [Fact]
+        public async Task RejectSuggestedDiscussionAsync_ReturnsOk()
+        {
+            var id = new Guid("0629b192-d453-4dc2-86f5-d8bbd7fb0955");
+            var response = await client.DeleteAsync($"api/SuggestDiscussion/RejectSuggestedDiscussion/{id}");
+
+            response.EnsureSuccessStatusCode();
+            var resp = await client.GetAsync($"api/SuggestDiscussion/GetSuggestedDiscussionById?id={id}");
+            Assert.Equal(HttpStatusCode.BadRequest, resp.StatusCode);
         }
     }
 }
