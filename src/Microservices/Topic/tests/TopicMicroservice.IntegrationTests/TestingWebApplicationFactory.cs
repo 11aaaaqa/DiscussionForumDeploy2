62e25386030e0bdc602a43bc07da1c5222615eee
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-31 18:13:02 +0400

    Disable logging

--- src/Microservices/Topic/tests/TopicMicroservice.IntegrationTests/TestingWebApplicationFactory.cs
+++ src/Microservices/Topic/tests/TopicMicroservice.IntegrationTests/TestingWebApplicationFactory.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using TopicMicroservice.Api.Database;
 using TopicMicroservice.Api.Models;
 
@@ -16,6 +17,7 @@ namespace TopicMicroservice.IntegrationTests
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            builder.ConfigureLogging(x => x.ClearProviders());
             builder.ConfigureServices(services =>
             {
                 var descriptor =
