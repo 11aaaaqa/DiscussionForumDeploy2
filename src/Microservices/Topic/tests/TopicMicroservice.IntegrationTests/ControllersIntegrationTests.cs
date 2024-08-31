50bfdfb8144bfb83f9fcd9a2dd67985045c21956
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-31 18:25:05 +0400

    Refactoring

--- src/Microservices/Topic/tests/TopicMicroservice.IntegrationTests/ControllersIntegrationTests.cs
+++ src/Microservices/Topic/tests/TopicMicroservice.IntegrationTests/ControllersIntegrationTests.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http.Json;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using TopicMicroservice.Api.DTOs;
 using TopicMicroservice.Api.Models;
 
