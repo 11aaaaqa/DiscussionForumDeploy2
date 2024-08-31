db1e2c6a58d68cfab9a38e916ae8381f20816df4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-31 11:39:40 +0400

    Add tests

--- src/Microservices/Discussion/tests/DiscussionMicroservice.IntegrationTests/DiscussionControllerIntegrationTests.cs
+++ src/Microservices/Discussion/tests/DiscussionMicroservice.IntegrationTests/DiscussionControllerIntegrationTests.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+using DiscussionMicroservice.Api.Database;
 using DiscussionMicroservice.Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,9 +24,34 @@ namespace DiscussionMicroservice.IntegrationTests
         [Fact]
         public async Task GetDiscussionByIdAsync_ReturnsBadRequest()
         {
-            var response = await client.GetAsync($"/api/Discussion/GetDiscussionById?id={Guid.Empty}");
+            var response = await client.GetAsync($"api/Discussion/GetDiscussionById?id={Guid.Empty}");
 
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetDiscussionByIdAsync_ReturnsOkWithDiscussion()
+        {
+            Guid id = new Guid("8077167c-d724-4258-8451-b617dc4bdfec");
+            var response = await client.GetAsync($"api/Discussion/GetDiscussionById?id={id}");
+
+            response.EnsureSuccessStatusCode();
+            var discussion = await response.Content.ReadFromJsonAsync<Discussion>();
+            Assert.NotNull(discussion);
+            Assert.Equal(id, discussion.Id);
+        }
+
+        [Fact]
+        public async Task GetDiscussionsByTopicName_ReturnsOkWithListOfDiscussions()
+        {
+            var topicName = "TestTopicName";
+            var response = await client.GetAsync($"api/Discussion/GetDiscussionsByTopicName?topicName={topicName}");
+
+            response.EnsureSuccessStatusCode();
+            var discussions = await response.Content.ReadFromJsonAsync<List<Discussion>>();
+            Assert.Equal(2, discussions.Count);
+            Assert.Equal(topicName, discussions[0].TopicName);
+            Assert.Equal(topicName, discussions[1].TopicName);
         }
     }
 }
