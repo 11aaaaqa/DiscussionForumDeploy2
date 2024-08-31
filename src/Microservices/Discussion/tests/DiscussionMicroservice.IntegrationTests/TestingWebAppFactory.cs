45f9cb277fb7351fd9c9931faa4e9c9a97686bea
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-08-31 11:39:36 +0400

    Update factory

--- src/Microservices/Discussion/tests/DiscussionMicroservice.IntegrationTests/TestingWebAppFactory.cs
+++ src/Microservices/Discussion/tests/DiscussionMicroservice.IntegrationTests/TestingWebAppFactory.cs
@@ -1,8 +1,10 @@
 ﻿using DiscussionMicroservice.Api.Database;
+using DiscussionMicroservice.Api.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DiscussionMicroservice.IntegrationTests
 {
@@ -10,8 +12,7 @@ namespace DiscussionMicroservice.IntegrationTests
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            base.ConfigureWebHost(builder);
-
+            builder.ConfigureLogging(x => x.ClearProviders());
             builder.ConfigureServices(services =>
             {
                 var descriptor = services.SingleOrDefault(
@@ -19,9 +20,9 @@ namespace DiscussionMicroservice.IntegrationTests
                          typeof(DbContextOptions<ApplicationDbContext>));
                 if (descriptor is not null)
                     services.Remove(descriptor);
-
+                
                 services.AddDbContext<ApplicationDbContext>(x => x.UseInMemoryDatabase("DiscussionControllerTests"));
-
+                
                 var sp = services.BuildServiceProvider();
                 using var scope = sp.CreateScope();
                 using var appContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
@@ -34,6 +35,36 @@ namespace DiscussionMicroservice.IntegrationTests
                 {
                     throw new Exception("Database wasn't created");
                 }
+
+                appContext.Discussions.Add(new Discussion
+                {
+                    Content = "TestContent",
+                    CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow),
+                    CreatedBy = "TestCreatedBy", Id = new Guid("8077167c-d724-4258-8451-b617dc4bdfec"), Rating = 123,
+                    TopicName = "TestTopicName",
+                    Title = "TestTitle"
+                });
+                appContext.Discussions.Add(new Discussion
+                {
+                    Content = "TestContent2",
+                    CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow),
+                    CreatedBy = "TestCreatedBy2",
+                    Id = Guid.NewGuid(),
+                    Rating = -224,
+                    TopicName = "TestTopicName",
+                    Title = "TestTitle2"
+                });
+                appContext.Discussions.Add(new Discussion
+                {
+                    Content = "TestContent3",
+                    CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow),
+                    CreatedBy = "TestCreatedBy3",
+                    Id = Guid.NewGuid(),
+                    Rating = 1,
+                    TopicName = "TestTopicName2",
+                    Title = "TestTitle3"
+                });
+                appContext.SaveChanges();
             });
         }
     }
