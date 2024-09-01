80c2973ffa862f659e134e50ad85bdb15e3725f4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-01 16:37:59 +0400

    Add database context

---
+++ src/Microservices/Comment/CommentMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CommentMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
