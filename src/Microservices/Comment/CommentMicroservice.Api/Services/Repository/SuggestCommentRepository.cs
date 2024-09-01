80f70d2fe89357ee2186cd423395a4df7c9cdd20
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-01 17:11:34 +0400

    Implement suggest comment repository

---
+++ src/Microservices/Comment/CommentMicroservice.Api/Services/Repository/SuggestCommentRepository.cs
@@ -0,0 +1,44 @@
+﻿using CommentMicroservice.Api.Database;
+using CommentMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentMicroservice.Api.Services.Repository
+{
+    public class SuggestCommentRepository : IRepository<SuggestedComment>
+    {
+        private readonly ApplicationDbContext context;
+
+        public SuggestCommentRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<SuggestedComment>> GetAllAsync() => await context.SuggestedComments.ToListAsync();
+
+        public async Task<List<SuggestedComment>> GetByDiscussionIdAsync(Guid id) =>
+            await context.SuggestedComments.Where(x => x.DiscussionId == id).ToListAsync();
+
+        public async Task<SuggestedComment?> GetByIdAsync(Guid id) =>
+            await context.SuggestedComments.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<SuggestedComment> UpdateAsync(SuggestedComment model)
+        {
+            context.SuggestedComments.Update(model);
+            await context.SaveChangesAsync();
+            return model;
+        }
+
+        public async Task<SuggestedComment> CreateAsync(SuggestedComment model)
+        {
+            await context.SuggestedComments.AddAsync(model);
+            await context.SaveChangesAsync();
+            return model;
+        }
+
+        public async Task DeleteByIdAsync(Guid id)
+        {
+            context.SuggestedComments.Remove(new SuggestedComment { Id = id });
+            await context.SaveChangesAsync();
+        }
+    }
+}
