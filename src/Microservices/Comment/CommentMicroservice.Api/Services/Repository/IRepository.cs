72bb8a92bbefb7fd1c219e5815e6af13b0c4aaa1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-01 16:51:17 +0400

    Add repos interface

---
+++ src/Microservices/Comment/CommentMicroservice.Api/Services/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace CommentMicroservice.Api.Services.Repository
+{
+    public interface IRepository<TModel>
+    {
+        Task<List<TModel>> GetAllAsync();
+        Task<List<TModel>> GetByDiscussionIdAsync(Guid id);
+        Task<TModel> GetByIdAsync(Guid id);
+        Task<TModel> UpdateAsync(TModel model);
+        Task<TModel> CreateAsync(TModel model);
+        Task DeleteByIdAsync(Guid id);
+    }
+}
