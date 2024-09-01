fcf8048a57f27005c77fdcc1c2e898de03b4af3d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-01 16:57:45 +0400

    Add nullable

--- src/Microservices/Comment/CommentMicroservice.Api/Services/Repository/IRepository.cs
+++ src/Microservices/Comment/CommentMicroservice.Api/Services/Repository/IRepository.cs
@@ -4,7 +4,7 @@
     {
         Task<List<TModel>> GetAllAsync();
         Task<List<TModel>> GetByDiscussionIdAsync(Guid id);
-        Task<TModel> GetByIdAsync(Guid id);
+        Task<TModel?> GetByIdAsync(Guid id);
         Task<TModel> UpdateAsync(TModel model);
         Task<TModel> CreateAsync(TModel model);
         Task DeleteByIdAsync(Guid id);
