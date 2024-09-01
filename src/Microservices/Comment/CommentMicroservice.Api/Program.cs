a13e533894d67b1981f13fe3798e09eca4506055
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-01 16:38:14 +0400

    Register database context

--- src/Microservices/Comment/CommentMicroservice.Api/Program.cs
+++ src/Microservices/Comment/CommentMicroservice.Api/Program.cs
@@ -1,5 +1,10 @@
+using CommentMicroservice.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseNpgsql(
+    builder.Configuration["Database:ConnectionString"]));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
