bd9f7bbec873d0b436730a03f0bf6469cabfcc58
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-01 17:13:22 +0400

    Add dto

---
+++ src/Microservices/Comment/CommentMicroservice.Api/DTOs/SuggestCommentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CommentMicroservice.Api.DTOs
+{
+    public class SuggestCommentDto
+    {
+        public string CreatedBy { get; set; }
+        public string Content { get; set; }
+        public Guid DiscussionId { get; set; }
+    }
+}
