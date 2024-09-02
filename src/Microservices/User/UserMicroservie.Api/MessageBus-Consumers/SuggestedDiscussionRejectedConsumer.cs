876434e9794a856de0aca3b978a706e045a69b1f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 18:14:35 +0400

    Add SuggestedDiscussionRejected message consumer

---
+++ src/Microservices/User/UserMicroservie.Api/MessageBus-Consumers/SuggestedDiscussionRejectedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.MessageBus_Consumers
+{
+    public class SuggestedDiscussionRejectedConsumer : IConsumer<ISuggestedDiscussionRejected>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public SuggestedDiscussionRejectedConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<ISuggestedDiscussionRejected> context)
+        {
+            var user = await databaseContext.Users.SingleOrDefaultAsync(x => x.UserName == context.Message.SuggestedBy);
+            if (user is not null)
+            {
+                user.SuggestedDiscussionsIds.Remove(context.Message.SuggestedDiscussionId);
+                databaseContext.Users.Update(user);
+                await databaseContext.SaveChangesAsync();
+            }
+        }
+    }
+}
