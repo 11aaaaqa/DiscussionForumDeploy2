dfba85d44acce9db742fa2ae939511036c22814a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 18:54:28 +0400

    Add message bus contract

---
+++ src/Building-blocks/MessageBus/Messages/ISuggestedCommentAccepted.cs
@@ -0,0 +1,8 @@
+﻿namespace MessageBus.Messages
+{
+    public interface ISuggestedCommentAccepted
+    {
+        public Guid AcceptedCommentId { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
