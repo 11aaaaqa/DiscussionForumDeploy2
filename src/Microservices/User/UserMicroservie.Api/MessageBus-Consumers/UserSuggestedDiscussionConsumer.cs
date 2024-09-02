c0d18f3cbd5a21f56a5d6241951234c3594efe2a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 16:01:09 +0400

    Add message consumer

---
+++ src/Microservices/User/UserMicroservie.Api/MessageBus-Consumers/UserSuggestedDiscussionConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.MessageBus_Consumers
+{
+    public class UserSuggestedDiscussionConsumer : IConsumer<IUserSuggestedDiscussion>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserSuggestedDiscussionConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserSuggestedDiscussion> context)
+        {
+            var user = await databaseContext.Users.SingleOrDefaultAsync(x => x.UserName == context.Message.CreatedBy);
+            if (user is not null)
+            {
+                user.SuggestedDiscussionsIds.Add(context.Message.SuggestedDiscussionId);
+                databaseContext.Users.Update(user);
+                await databaseContext.SaveChangesAsync();
+            }
+        }
+    }
+}
