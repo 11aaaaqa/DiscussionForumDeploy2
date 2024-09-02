776872020b04a771ac3fc12e98df072b73d4fb52
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 15:42:13 +0400

    Add migration

---
+++ src/Microservices/User/UserMicroservie.Api/Database/Migrations/20240902113841_RemoveAvatarFromUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAvatarFromUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Avatar",
+                table: "Users",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
