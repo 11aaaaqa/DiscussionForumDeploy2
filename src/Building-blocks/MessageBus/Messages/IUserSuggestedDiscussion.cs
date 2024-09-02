e14b879318d649f3e0a3c6e51141718716bfe1c2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 15:59:52 +0400

    Add message contract

---
+++ src/Building-blocks/MessageBus/Messages/IUserSuggestedDiscussion.cs
@@ -0,0 +1,8 @@
+﻿namespace MessageBus.Messages
+{
+    public interface IUserSuggestedDiscussion
+    {
+        public Guid SuggestedDiscussionId { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
