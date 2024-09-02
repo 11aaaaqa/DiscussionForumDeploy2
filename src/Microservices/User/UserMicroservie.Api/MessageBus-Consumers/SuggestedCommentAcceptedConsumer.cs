cc494cca6982969f3016c730c926c90105780a72
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 18:54:49 +0400

    Add message bus message consumer

---
+++ src/Microservices/User/UserMicroservie.Api/MessageBus-Consumers/SuggestedCommentAcceptedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.MessageBus_Consumers
+{
+    public class SuggestedCommentAcceptedConsumer : IConsumer<ISuggestedCommentAccepted>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public SuggestedCommentAcceptedConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<ISuggestedCommentAccepted> context)
+        {
+            var user = await databaseContext.Users.SingleOrDefaultAsync(x => x.UserName == context.Message.CreatedBy);
+            if (user is not null)
+            {
+                user.SuggestedCommentsIds.Remove(context.Message.AcceptedCommentId);
+                user.CommentsIds.Add(context.Message.AcceptedCommentId);
+                databaseContext.Users.Update(user);
+                await databaseContext.SaveChangesAsync();
+            }
+        }
+    }
+}
