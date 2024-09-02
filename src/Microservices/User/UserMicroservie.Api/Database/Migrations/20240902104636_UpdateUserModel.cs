361ff424b0d2e174abe7162e7c547e4b790305a2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 14:48:51 +0400

    Add migration

---
+++ src/Microservices/User/UserMicroservie.Api/Database/Migrations/20240902104636_UpdateUserModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Avatar",
+                table: "Users",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "CommentsIds",
+                table: "Users",
+                type: "uuid[]",
+                nullable: false);
+
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "CreatedDiscussionsIds",
+                table: "Users",
+                type: "uuid[]",
+                nullable: false);
+
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "SuggestedDiscussionsIds",
+                table: "Users",
+                type: "uuid[]",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CommentsIds",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDiscussionsIds",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SuggestedDiscussionsIds",
+                table: "Users");
+        }
+    }
+}
