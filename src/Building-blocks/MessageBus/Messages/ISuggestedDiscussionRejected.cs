819cde9839fe388c31f83972b9f4ecfafe9505e2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 18:13:49 +0400

    Add message contract

---
+++ src/Building-blocks/MessageBus/Messages/ISuggestedDiscussionRejected.cs
@@ -0,0 +1,8 @@
+﻿namespace MessageBus.Messages
+{
+    public interface ISuggestedDiscussionRejected
+    {
+        public Guid SuggestedDiscussionId { get; set; }
+        public string SuggestedBy { get; set; }
+    }
+}
