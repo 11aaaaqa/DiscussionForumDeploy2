82cee8b009e8a1c3fd0337b38284785ca70c0c04
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 18:36:03 +0400

    Add migration

--- src/Microservices/User/UserMicroservie.Api/Database/Migrations/20240902143415_AddSuggestedCommentsIdsListColumn.Designer.cs
+++ src/Microservices/User/UserMicroservie.Api/Database/Migrations/20240902143415_AddSuggestedCommentsIdsListColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UserMicroservice.Api.Database;
@@ -12,9 +13,11 @@ using UserMicroservice.Api.Database;
 namespace UserMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240902143415_AddSuggestedCommentsIdsListColumn")]
+    partial class AddSuggestedCommentsIdsListColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Microservices/User/UserMicroservie.Api/Database/Migrations/20240902143415_AddSuggestedCommentsIdsListColumn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSuggestedCommentsIdsListColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "SuggestedCommentsIds",
+                table: "Users",
+                type: "uuid[]",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SuggestedCommentsIds",
+                table: "Users");
+        }
+    }
+}
