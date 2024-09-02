cd1e1cb3a00877e0cae0147ecda276de1c702a79
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-02 18:23:29 +0400

    Add consumer

---
+++ src/Microservices/User/UserMicroservie.Api/MessageBus-Consumers/SuggestedDiscussionAcceptedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.MessageBus_Consumers
+{
+    public class SuggestedDiscussionAcceptedConsumer : IConsumer<ISuggestedDiscussionAccepted>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public SuggestedDiscussionAcceptedConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<ISuggestedDiscussionAccepted> context)
+        {
+            var user = await databaseContext.Users.SingleOrDefaultAsync(x => x.UserName == context.Message.CreatedBy);
+            if (user is not null)
+            {
+                user.SuggestedDiscussionsIds.Remove(context.Message.AcceptedDiscussionId);
+                user.CreatedDiscussionsIds.Add(context.Message.AcceptedDiscussionId);
+                databaseContext.Users.Update(user);
+                await databaseContext.SaveChangesAsync();
+            }
+        }
+    }
+}
