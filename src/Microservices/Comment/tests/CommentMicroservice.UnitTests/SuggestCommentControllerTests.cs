29cb1fc714867588232c1c52383e4c7dfcd139d6
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-03 14:47:59 +0400

    Add GetAllSuggestedCommentsAsync method test

---
+++ src/Microservices/Comment/tests/CommentMicroservice.UnitTests/SuggestCommentControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using CommentMicroservice.Api.Controllers;
+using CommentMicroservice.Api.Models;
+using CommentMicroservice.Api.Services.Repository;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CommentMicroservice.UnitTests
+{
+    public class SuggestCommentControllerTests
+    {
+        [Fact]
+        public async Task GetAllSuggestedCommentsAsync_ReturnsOkWithSuggestedComments()
+        {
+            var mock = new Mock<IRepository<SuggestedComment>>();
+            mock.Setup(x => x.GetAllAsync()).ReturnsAsync(new List<SuggestedComment>
+            {
+                new() {Id = It.IsAny<Guid>(), Content = It.IsAny<string>(), CreatedBy = It.IsAny<string>(),
+                    CreatedDate = It.IsAny<DateTime>(), DiscussionId = It.IsAny<Guid>()},
+                new() {Id = It.IsAny<Guid>(), Content = It.IsAny<string>(), CreatedBy = It.IsAny<string>(),
+                    CreatedDate = It.IsAny<DateTime>(), DiscussionId = It.IsAny<Guid>()},
+                new() {Id = It.IsAny<Guid>(), Content = It.IsAny<string>(), CreatedBy = It.IsAny<string>(),
+                    CreatedDate = It.IsAny<DateTime>(), DiscussionId = It.IsAny<Guid>()}
+            });
+            var controller = new SuggestCommentController(mock.Object, new Mock<IRepository<Comment>>().Object,
+                new Mock<IPublishEndpoint>().Object);
+
+            var result = await controller.GetAllSuggestedCommentsAsync();
+
+            var methodResult = Assert.IsType<OkObjectResult>(result);
+            var comments = Assert.IsType<List<SuggestedComment>>(methodResult.Value);
+            Assert.Equal(3, comments.Count);
+        }
+    }
+}
