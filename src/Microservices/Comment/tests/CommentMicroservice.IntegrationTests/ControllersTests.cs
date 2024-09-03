17d87936d6887297a266b23bd1bd2560db92e468
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-03 15:43:54 +0400

    Add test for SuggestCommentAsync method

---
+++ src/Microservices/Comment/tests/CommentMicroservice.IntegrationTests/ControllersTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CommentMicroservice.Api.DTOs;
+using CommentMicroservice.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommentMicroservice.IntegrationTests
+{
+    public class ControllersTests : IClassFixture<TestingWebAppFactory<Program>>
+    {
+        private readonly HttpClient client;
+
+        public ControllersTests(TestingWebAppFactory<Program> factory)
+        {
+            client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task SuggestCommentAsync_ReturnsOkWithSuggestedComment()
+        {
+            var model = new SuggestCommentDto
+                { Content = "Content", CreatedBy = "CreatedBy", DiscussionId = Guid.NewGuid() };
+            using StringContent jsonContent = new(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json");
+            var result = await client.PostAsync("api/SuggestComment/Suggest", jsonContent);
+
+            result.EnsureSuccessStatusCode();
+            var suggestedComment = await result.Content.ReadFromJsonAsync<SuggestedComment>();
+            Assert.NotNull(suggestedComment);
+            Assert.Equal(model.Content, suggestedComment.Content);
+            Assert.Equal(model.CreatedBy, suggestedComment.CreatedBy);
+            Assert.Equal(model.DiscussionId, suggestedComment.DiscussionId);
+        }
+    }
+}
