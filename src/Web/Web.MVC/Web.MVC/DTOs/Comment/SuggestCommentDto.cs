7fe705f7e4c71ea5f8cea106cf34753c05ecd0b0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-05 17:06:26 +0400

    Add dto

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/Comment/SuggestCommentDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Comment
+{
+    public class SuggestCommentDto
+    {
+        public string? CreatedBy { get; set; }
+
+        [Required]
+        [Display(Name = "Оставить комментарий")]
+        public string Content { get; set; }
+    }
+}
