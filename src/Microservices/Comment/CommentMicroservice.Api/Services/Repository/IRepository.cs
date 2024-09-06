48aa5c58d071b143d25cde9aa1c2fa0795655ed3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-06 11:51:52 +0400

    Update

--- src/Microservices/Comment/CommentMicroservice.Api/Services/Repository/IRepository.cs
+++ src/Microservices/Comment/CommentMicroservice.Api/Services/Repository/IRepository.cs
@@ -3,7 +3,7 @@
     public interface IRepository<TModel>
     {
         Task<List<TModel>> GetAllAsync();
-        Task<List<TModel>> GetByDiscussionIdAsync(Guid id);
+        Task<List<TModel>?> GetByDiscussionIdAsync(Guid id);
         Task<TModel?> GetByIdAsync(Guid id);
         Task<TModel> UpdateAsync(TModel model);
         Task<TModel> CreateAsync(TModel model);
