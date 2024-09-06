c35593bdb24c08d3b96a35103e69c2b8ab7334d0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-06 14:36:44 +0400

    Add comment deleting post method

---
+++ src/Web/Web.MVC/Web.MVC/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.MVC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public CommentController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> DeleteComment(Guid id, string returnUrl)
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.DeleteAsync($"http://comment-microservice-api:8080/api/Comment/DeleteCommentById/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                if (!string.IsNullOrEmpty(returnUrl))
+                    return LocalRedirect(returnUrl);
+                return RedirectToAction("Topics", "Topic");
+            }
+
+            return View("ActionError");
+        }
+    }
+}
