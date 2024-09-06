a9ddc08da93bc7c468958baeb846211420dd41ba
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-06 12:08:14 +0400

    Add message bus message consumer

---
+++ src/Microservices/User/UserMicroservie.Api/MessageBus-Consumers/CommentConsumers/CommentCreatedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.MessageBus_Consumers.CommentConsumers
+{
+    public class CommentCreatedConsumer : IConsumer<ICommentCreated>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public CommentCreatedConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<ICommentCreated> context)
+        {
+            var user = await databaseContext.Users.SingleAsync(x => x.UserName == context.Message.CreatedBy);
+            user.CommentsIds.Add(context.Message.CommentId);
+            databaseContext.Users.Update(user);
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
