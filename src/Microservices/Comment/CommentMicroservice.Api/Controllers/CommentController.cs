c5929f862a8100ae85e09dc4e5131a1c0012accd
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-06 12:02:11 +0400

    Add comment controller

---
+++ src/Microservices/Comment/CommentMicroservice.Api/Controllers/CommentController.cs
@@ -0,0 +1,84 @@
+﻿using CommentMicroservice.Api.Models;
+using CommentMicroservice.Api.Services.Repository;
+using MassTransit;
+using MessageBus.Messages;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.Design;
+
+namespace CommentMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly IRepository<Comment> repository;
+        private readonly IPublishEndpoint publishEndpoint;
+
+        public CommentController(IRepository<Comment> repository, IPublishEndpoint publishEndpoint)
+        {
+            this.repository = repository;
+            this.publishEndpoint = publishEndpoint;
+        }
+
+        [Route("GetAllComments")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllCommentsAsync() => Ok(await repository.GetAllAsync());
+
+        [Route("GetCommentsByDiscussionId/{discussionId}")]
+        public async Task<IActionResult> GetCommentsByDiscussionIdAsync(Guid discussionId)
+        {
+            var comments = await repository.GetByDiscussionIdAsync(discussionId);
+            if (comments is null)
+            {
+                return BadRequest();
+            }
+            return Ok(comments);
+        }
+
+        [Route("UpdateComment")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateCommentAsync([FromBody] Comment model)
+        {
+            var comment = await repository.GetByIdAsync(model.Id);
+            if (comment is null)
+               return BadRequest();
+
+            return Ok(await repository.UpdateAsync(model));
+        }
+
+        [Route("DeleteCommentById/{id}")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCommentByIdAsync(Guid id)
+        {
+            var comment = await repository.GetByIdAsync(id);
+            if (comment is not null)
+            {
+                await repository.DeleteByIdAsync(id);
+
+                await publishEndpoint.Publish<ICommentDeleted>(new
+                {
+                    CommentId = comment.Id,
+                    CommentCreatedBy = comment.CreatedBy
+                });
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [Route("CreateComment")]
+        [HttpPost]
+        public async Task<IActionResult> CreateCommentAsync([FromBody] Comment model)
+        {
+            var createdComment = await repository.CreateAsync(model);
+
+            await publishEndpoint.Publish<ICommentCreated>(new
+            {
+                CommentId = createdComment.Id, createdComment.CreatedBy
+            });
+
+            return Ok(createdComment);
+        }
+        
+    }
+}
