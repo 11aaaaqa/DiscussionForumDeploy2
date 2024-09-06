c7c798ed032e471cef389daf1d1dddd12cf8dc17
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-06 12:02:20 +0400

    Add message contracts

---
+++ src/Building-blocks/MessageBus/Messages/ICommentCreated.cs
@@ -0,0 +1,8 @@
+﻿namespace MessageBus.Messages
+{
+    public interface ICommentCreated
+    {
+        public Guid CommentId { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ src/Building-blocks/MessageBus/Messages/ICommentDeleted.cs
@@ -0,0 +1,8 @@
+﻿namespace MessageBus.Messages
+{
+    public interface ICommentDeleted
+    {
+        public Guid CommentId { get; set; }
+        public string CommentCreatedBy { get; set; }
+    }
+}
