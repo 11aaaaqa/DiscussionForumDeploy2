98eb58e9f660fab5b86c7000c94e3175112ce40b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-07 13:44:28 +0400

    Add tests for GetReportByIdAsync method

--- src/Microservices/Report/tests/ReportMicroservice.UnitTests/ReportControllerTests.cs
+++ src/Microservices/Report/tests/ReportMicroservice.UnitTests/ReportControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
 using Moq;
 using ReportMicroservice.Api.Controllers;
 using ReportMicroservice.Api.Models;
@@ -40,6 +41,38 @@ namespace ReportMicroservice.UnitTests
             var reports = Assert.IsType<List<Report>>(methodResult.Value);
             Assert.Equal(3, reports.Count);
             mock.Verify(x => x.GetAllReportsAsync());
+        }
+
+        [Fact]
+        public async Task GetReportByIdAsync_ReturnsBadRequest()
+        {
+            var id = It.IsAny<Guid>();
+            var mock = new Mock<IReportService<Report>>();
+            mock.Setup(x => x.GetReportByIdAsync(id)).ReturnsAsync((Report?)null);
+            var controller = new ReportController(mock.Object);
+
+            var result = await controller.GetReportByIdAsync(id);
+
+            Assert.IsType<BadRequestResult>(result);
+            mock.Verify(x => x.GetReportByIdAsync(id));
+        }
+
+        [Fact]
+        public async Task GetReportByIdAsync_ReturnsOkWithReport()
+        {
+            var id = It.IsAny<Guid>();
+            var mock = new Mock<IReportService<Report>>();
+            mock.Setup(x => x.GetReportByIdAsync(id)).ReturnsAsync(new Report { Id = id });
+            var controller = new ReportController(mock.Object);
+
+            var result = await controller.GetReportByIdAsync(id);
+
+            var methodResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, methodResult.StatusCode);
+            Assert.NotNull(methodResult.Value);
+            var report = Assert.IsType<Report>(methodResult.Value);
+            Assert.Equal(id, report.Id);
+            mock.Verify(x => x.GetReportByIdAsync(id));
         }
     }
 }
