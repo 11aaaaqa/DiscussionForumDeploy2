58cffdea5435f8984d0e7a85e427b0d11261c1f2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-07 12:14:23 +0400

    Add database context

---
+++ src/Microservices/Report/ReportMicroservice/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportMicroservice.Models;
+
+namespace ReportMicroservice.Database
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Report> Reports { get; set; }
+    }
+}
