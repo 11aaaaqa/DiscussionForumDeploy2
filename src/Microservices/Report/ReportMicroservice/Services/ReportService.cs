f0f6085659406deba2a622b34deb90586c25a6ab
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-07 13:04:48 +0400

    Implement report service interface

---
+++ src/Microservices/Report/ReportMicroservice/Services/ReportService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportMicroservice.Database;
+using ReportMicroservice.Models;
+
+namespace ReportMicroservice.Services
+{
+    public class ReportService : IReportService<Report>
+    {
+        private readonly ApplicationDbContext context;
+
+        public ReportService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Report>> GetAllReportsAsync() => await context.Reports.ToListAsync();
+
+
+        public async Task<Report?> GetReportByIdAsync(Guid reportId) =>
+            await context.Reports.SingleOrDefaultAsync(x => x.Id == reportId);
+
+        public async Task<Report> CreateReportAsync(Report model)
+        {
+            await context.Reports.AddAsync(model);
+            await context.SaveChangesAsync();
+            return model;
+        }
+
+        public async Task DeleteReportsByUserName(string userName)
+        {
+            var reports = await context.Reports.Where(x => x.UserNameReportedBy == userName).ToListAsync();
+            foreach (var report in reports)
+            {
+                context.Reports.Remove(report);
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
