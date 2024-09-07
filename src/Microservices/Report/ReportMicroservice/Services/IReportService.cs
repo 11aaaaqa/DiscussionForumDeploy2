ff15e0624b022a046f82f513b180825467154d71
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-07 12:57:53 +0400

    Add report service interface

---
+++ src/Microservices/Report/ReportMicroservice/Services/IReportService.cs
@@ -0,0 +1,12 @@
+﻿using ReportMicroservice.Models;
+
+namespace ReportMicroservice.Services
+{
+    public interface IReportService<TModel>
+    {
+        Task<List<TModel>> GetAllReportsAsync();
+        Task<TModel> GetReportByIdAsync(Guid reportId);
+        Task<Report> CreateReportAsync(TModel model);
+        Task DeleteReportsByUserId(Guid userId);
+    }
+}
