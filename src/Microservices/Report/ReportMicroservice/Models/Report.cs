614f09d57764c62c877f01c5ad68a22f683256bc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-07 12:14:16 +0400

    Add model

---
+++ src/Microservices/Report/ReportMicroservice/Models/Report.cs
@@ -0,0 +1,11 @@
+﻿namespace ReportMicroservice.Models
+{
+    public class Report
+    {
+        public Guid Id { get; set; }
+        public Guid IdReportedBy { get; set; }
+        public Guid? CommentId { get; set; }
+        public Guid? DiscussionId { get; set; }
+        public string Reason { get; set; }
+    }
+}
