39598cffaa2d0645ed66f3b82f4f2cd81534d665
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-07 13:18:28 +0400

    Add migration

---
+++ src/Microservices/Report/ReportMicroservice/Database/Migrations/20240907091755_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserNameReportedBy = table.Column<string>(type: "text", nullable: false),
+                    UserIdReportedTo = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReportedCommentContent = table.Column<string>(type: "text", nullable: true),
+                    ReportedDiscussionTitle = table.Column<string>(type: "text", nullable: true),
+                    ReportedDiscussionContent = table.Column<string>(type: "text", nullable: true),
+                    Reason = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+        }
+    }
+}
