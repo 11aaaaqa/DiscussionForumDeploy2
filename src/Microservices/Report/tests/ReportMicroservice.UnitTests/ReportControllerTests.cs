ee40bc500ec0aa875467b93965240a30a2f5eb31
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-07 13:38:02 +0400

    Add test for GetAllReportsAsync method

---
+++ src/Microservices/Report/tests/ReportMicroservice.UnitTests/ReportControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ReportMicroservice.Api.Controllers;
+using ReportMicroservice.Api.Models;
+using ReportMicroservice.Api.Services;
+
+namespace ReportMicroservice.UnitTests
+{
+    public class ReportControllerTests
+    {
+        [Fact]
+        public async Task GetAllReportsAsync_ReturnsOkWithListOfReports()
+        {
+            var mock = new Mock<IReportService<Report>>();
+            mock.Setup(x => x.GetAllReportsAsync()).ReturnsAsync(new List<Report>
+            {
+                new()
+                {
+                    Id = It.IsAny<Guid>(),Reason = It.IsAny<string>(), ReportedCommentContent = It.IsAny<string>(), UserIdReportedTo = It.IsAny<Guid>(),
+                    UserNameReportedBy = It.IsAny<string>()
+                },
+                new()
+                {
+                    Id = It.IsAny<Guid>(),Reason = It.IsAny<string>(), ReportedCommentContent = It.IsAny<string>(), UserIdReportedTo = It.IsAny<Guid>(),
+                    UserNameReportedBy = It.IsAny<string>()
+                },
+                new()
+                {
+                    Id = It.IsAny<Guid>(),Reason = It.IsAny<string>(), UserIdReportedTo = It.IsAny<Guid>(), UserNameReportedBy = It.IsAny<string>(),
+                    ReportedDiscussionContent = It.IsAny<string>(), ReportedDiscussionTitle = It.IsAny<string>()
+                }
+            });
+            var controller = new ReportController(mock.Object);
+
+            var result = await controller.GetAllReportsAsync();
+
+            var methodResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, methodResult.StatusCode);
+            Assert.NotNull(methodResult.Value);
+            var reports = Assert.IsType<List<Report>>(methodResult.Value);
+            Assert.Equal(3, reports.Count);
+            mock.Verify(x => x.GetAllReportsAsync());
+        }
+    }
+}
