c0b77e6adb8a1e66a233f8f2756be39d0feae19b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 12:07:55 +0400

    Add migration

---
+++ src/Microservices/Report/ReportMicroservice/Database/Migrations/20240908080609_AddReportTypeColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReportTypeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReportType",
+                table: "Reports",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReportType",
+                table: "Reports");
+        }
+    }
+}
