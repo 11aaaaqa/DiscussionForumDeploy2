26afac7f57a0b8d126b1e9a7b6aedbbebb441d9c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 13:01:01 +0400

    Add api request model

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiRequests/CreateReportRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.MVC.Models.ApiRequests
+{
+    public class CreateReportRequest
+    {
+        public string UserNameReportedBy { get; set; }
+        public Guid UserIdReportedTo { get; set; }
+        public string? ReportedCommentContent { get; set; }
+        public string? ReportedDiscussionTitle { get; set; }
+        public string? ReportedDiscussionContent { get; set; }
+        public string Reason { get; set; }
+        public string ReportType { get; set; }
+    }
+}
