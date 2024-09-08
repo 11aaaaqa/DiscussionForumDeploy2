366e9d3fe9a3b945da904cc7dc224eac5e5b3257
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 19:08:21 +0400

    Add migration

---
+++ src/Microservices/User/UserMicroservie.Api/Database/Migrations/20240908150753_AddBanColumns.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBanColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BanType",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BannedFor",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BannedUntil",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBanned",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BanType",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BannedFor",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BannedUntil",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsBanned",
+                table: "Users");
+        }
+    }
+}
