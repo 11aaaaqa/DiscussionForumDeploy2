1ae90e94833c2bf6fbf1fc96ca1fad81eca725ad
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 19:04:44 +0400

    Add user service interface

---
+++ src/Microservices/User/UserMicroservie.Api/Services/User/IUserService.cs
@@ -0,0 +1,12 @@
+﻿namespace UserMicroservice.Api.Services.User
+{
+    public interface IUserService<TUser> where TUser : class
+    {
+        Task<TUser?> GetUserByIdAsync(Guid id);
+        Task<TUser?> GetUserByUserName(string userName);
+        Task<List<Guid>?> GetCreatedDiscussionsIdsByUserIdAsync(Guid id);
+        Task<List<Guid>?> GetSuggestedDiscussionsIdsByUserIdAsync(Guid id);
+        Task<List<Guid>?> GetSuggestedCommentsIdsByUserIdAsync(Guid id);
+        Task<List<Guid>?> GetCommentsIdsByUserIdAsync(Guid id);
+    }
+}
