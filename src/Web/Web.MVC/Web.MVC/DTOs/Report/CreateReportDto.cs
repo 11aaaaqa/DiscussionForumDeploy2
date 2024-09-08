8392109764df5087bd20c240ce0ac9411b768143
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 13:00:52 +0400

    Add dto

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/Report/CreateReportDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.Report
+{
+    public class CreateReportDto
+    {
+        public string ReportedUserName { get; set; }
+        public string? ReportedCommentContent { get; set; }
+        public string? ReportedDiscussionTitle { get; set; }
+        public string? ReportedDiscussionContent { get; set; }
+
+        [Required]
+        [Display(Name="Причина")]
+        public string Reason { get; set; }
+    }
+}
