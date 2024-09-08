64a67c7393bec4f945dbe54019d03d34ceb160aa
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 13:28:47 +0400

    Add test for GetReportsByReportTypeAsync method

--- src/Microservices/Report/tests/ReportMicroservice.UnitTests/ReportControllerTests.cs
+++ src/Microservices/Report/tests/ReportMicroservice.UnitTests/ReportControllerTests.cs
@@ -108,5 +108,67 @@ namespace ReportMicroservice.UnitTests
             Assert.Equal(200, methodResult.StatusCode);
             mock.VerifyAll();
         }
+
+        [Fact]
+        public async Task GetReportsByReportTypeAsync_ReturnsBadRequest()
+        {
+            var reportType = It.IsAny<string>();
+            var mock = new Mock<IReportService<Report>>();
+            mock.Setup(x => x.GetReportsByReportType(reportType)).ReturnsAsync((List<Report>?)null);
+            var controller = new ReportController(mock.Object);
+
+            var result = await controller.GetReportsByReportTypeAsync(reportType);
+
+            Assert.IsType<BadRequestResult>(result);
+            mock.Verify(x => x.GetReportsByReportType(reportType));
+        }
+
+        [Fact]
+        public async Task GetReportsByReportTypeAsync_ReturnsOkWithListOfReports()
+        {
+            var reportType = It.IsAny<string>();
+            var mock = new Mock<IReportService<Report>>();
+            mock.Setup(x => x.GetReportsByReportType(reportType)).ReturnsAsync(new List<Report>
+            {
+                new()
+                {
+                    ReportType = reportType, Id = Guid.NewGuid(), Reason = It.IsAny<string>(), UserIdReportedTo = Guid.NewGuid(),
+                    UserNameReportedBy = It.IsAny<string>(), ReportedCommentContent = It.IsAny<string>()
+                },
+                new()
+                {
+                    ReportType = reportType, Id = Guid.NewGuid(), Reason = It.IsAny<string>(), UserIdReportedTo = Guid.NewGuid(),
+                    UserNameReportedBy = It.IsAny<string>(), ReportedDiscussionContent = It.IsAny<string>(), ReportedDiscussionTitle = It.IsAny<string>()
+                }
+            });
+            var controller = new ReportController(mock.Object);
+
+            var result = await controller.GetReportsByReportTypeAsync(reportType);
+
+            var methodResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, methodResult.StatusCode);
+            Assert.NotNull(methodResult.Value);
+            var reports = Assert.IsType<List<Report>>(methodResult.Value);
+            Assert.Equal(2, reports.Count);
+            mock.Verify(x => x.GetReportsByReportType(reportType));
+        }
+
+        [Fact]
+        public async Task GetReportsByReportTypeAsync_ReturnsOkWithListOfReportsThatContainsZeroObjects()
+        {
+            var reportType = It.IsAny<string>();
+            var mock = new Mock<IReportService<Report>>();
+            mock.Setup(x => x.GetReportsByReportType(reportType)).ReturnsAsync(new List<Report>());
+            var controller = new ReportController(mock.Object);
+
+            var result = await controller.GetReportsByReportTypeAsync(reportType);
+
+            var methodResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, methodResult.StatusCode);
+            Assert.NotNull(methodResult.Value);
+            var reports = Assert.IsType<List<Report>>(methodResult.Value);
+            Assert.Empty(reports);
+            mock.Verify(x => x.GetReportsByReportType(reportType));
+        }
     }
 }
