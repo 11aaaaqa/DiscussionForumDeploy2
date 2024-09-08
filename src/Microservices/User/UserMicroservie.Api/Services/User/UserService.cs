1e09fe9d846a0d19a05f8485c05c3b116bed5827
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 19:05:11 +0400

    Implement user service interface

---
+++ src/Microservices/User/UserMicroservie.Api/Services/User/UserService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.Services.User
+{
+    public class UserService : IUserService<Models.User>
+    {
+        private readonly ApplicationDbContext context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Models.User?> GetUserByIdAsync(Guid id) =>
+            await context.Users.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Models.User?> GetUserByUserName(string userName) =>
+            await context.Users.SingleOrDefaultAsync(x => x.UserName == userName);
+
+        public async Task<List<Guid>?> GetCreatedDiscussionsIdsByUserIdAsync(Guid id)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Id == id);
+            if (user is null)
+                return null;
+            return user.CreatedDiscussionsIds;
+        }
+
+        public async Task<List<Guid>?> GetSuggestedDiscussionsIdsByUserIdAsync(Guid id)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Id == id);
+            if (user is null)
+                return null;
+            return user.SuggestedDiscussionsIds;
+        }
+
+        public async Task<List<Guid>?> GetSuggestedCommentsIdsByUserIdAsync(Guid id)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Id == id);
+            if (user is null)
+                return null;
+            return user.SuggestedCommentsIds;
+        }
+
+        public async Task<List<Guid>?> GetCommentsIdsByUserIdAsync(Guid id)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Id == id);
+            if (user is null)
+                return null;
+            return user.CommentsIds;
+        }
+    }
+}
