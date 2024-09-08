05332d4ea1bc35ff6e97872ea7b248bf874cf98a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 13:00:24 +0400

    Add beta version of controller

---
+++ src/Microservices/User/UserMicroservie.Api/Controllers/CustUserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustUserController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public CustUserController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [Route("GetUserByUserName/{userName}")]
+        [HttpGet]
+        public async Task<IActionResult> GetUserByUserNameAsync(string userName)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.UserName == userName);
+            if (user is null)
+                return BadRequest();
+            return Ok(user);
+        }
+    }
+}
