bcfddc7e7dcc596f6d22d65657cff89a25f3e87b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-08 19:04:32 +0400

    Added ban service interface

---
+++ src/Microservices/User/UserMicroservie.Api/Services/Ban/IBanService.cs
@@ -0,0 +1,11 @@
+﻿namespace UserMicroservice.Api.Services.Ban
+{
+    public interface IBanService<TUser> where TUser : class
+    {
+        Task<bool> IsUserBannedAsync(Guid userId);
+        Task BanUserAsync(Guid userId, string reason, string banType, uint forDays);
+        Task BanUserAsync(string userName, string reason, string banType, uint forDays);
+        Task UnbanUserAsync(Guid userId);
+        Task UnbanUserAsync(string userName);
+    }
+}
