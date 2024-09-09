e4ed209df661d108362b85e357ac061ee5195245
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 13:47:28 +0400

    Add test for GetBansByUserNameAsync method

--- src/Microservices/BanHistory/tests/BanHistoryMicroservice.UnitTests/BanHistoryControllerTests.cs
+++ src/Microservices/BanHistory/tests/BanHistoryMicroservice.UnitTests/BanHistoryControllerTests.cs
@@ -93,5 +93,39 @@ namespace BanHistoryMicroservice.UnitTests
             }
             mock.Verify(x => x.GetByUserIdAsync(id));
         }
+
+        [Fact]
+        public async Task GetBansByUserNameAsync_ReturnsOkWithListOfBansWithNeededUserName()
+        {
+            var userName = It.IsAny<string>();
+            var mock = new Mock<IBanService<Ban>>();
+            mock.Setup(x => x.GetByUserNameAsync(userName)).ReturnsAsync(new List<Ban>
+            {
+                new ()
+                {
+                    BanType = It.IsAny<string>(), DurationInDays = It.IsAny<uint>(), Id = It.IsAny<Guid>(), UserName = userName,
+                    Reason = It.IsAny<string>(), UserId = It.IsAny<Guid>()
+                },
+                new ()
+                {
+                    BanType = It.IsAny<string>(), DurationInDays = It.IsAny<uint>(), Id = It.IsAny<Guid>(), UserName = userName,
+                    Reason = It.IsAny<string>(), UserId = It.IsAny<Guid>()
+                }
+            });
+            var controller = new BanHistoryController(mock.Object);
+
+            var result = await controller.GetBansByUserNameAsync(userName);
+
+            var methodResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(methodResult.Value);
+            Assert.Equal(200, methodResult.StatusCode);
+            var bans = Assert.IsType<List<Ban>>(methodResult.Value);
+            Assert.Equal(2, bans.Count);
+            foreach (var ban in bans)
+            {
+                Assert.Equal(userName, ban.UserName);
+            }
+            mock.Verify(x => x.GetByUserNameAsync(userName));
+        }
     }
 }
