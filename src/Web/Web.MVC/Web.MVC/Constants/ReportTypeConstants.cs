44c246f8339a2805277de2ab640a5c37bec3b501
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 17:57:41 +0400

    Add constants

---
+++ src/Web/Web.MVC/Web.MVC/Constants/ReportTypeConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Constants
+{
+    public static class ReportTypeConstants
+    {
+        public const string DiscussionType = "Обсуждение";
+        public const string CommentType = "Комментарий";
+    }
+}
