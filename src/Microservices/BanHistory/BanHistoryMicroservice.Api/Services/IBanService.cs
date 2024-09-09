6b037e82f1767fd1011440133304da9318c9b8ae
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 10:16:17 +0400

    Add ban service interface

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Services/IBanService.cs
@@ -0,0 +1,13 @@
+﻿namespace BanHistoryMicroservice.Api.Services
+{
+    public interface IBanService<TModel>
+    {
+        Task<List<TModel>> GetAllBansAsync();
+        Task<List<TModel>?> GetByUserIdAsync(Guid userId);
+        Task<List<TModel>?> GetByUserNameAsync(string userName);
+        Task<List<TModel>?> GetByBanTypeAsync(string banType);
+        Task<TModel?> GetByIdAsync(Guid id);
+        Task<TModel> CreateAsync(TModel model);
+        Task DeleteAsync(Guid id);
+    }
+}
