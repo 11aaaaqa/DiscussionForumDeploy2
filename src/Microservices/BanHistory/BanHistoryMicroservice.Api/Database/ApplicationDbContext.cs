65e28cca2d531b41c233636977e754612fc018b2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 10:10:38 +0400

    Add database context

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BanHistoryMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanHistoryMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Ban> Bans { get; set; }
+    }
+}
