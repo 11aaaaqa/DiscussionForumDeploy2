bbec0b6bbbb8eb554510999d0eac59ebdba8f282
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 11:31:29 +0400

    Add message bus message consumers

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/MessageBus/MessageBusConsumers/UserBannedByUserIdConsumer.cs
@@ -0,0 +1,25 @@
+﻿using BanHistoryMicroservice.Api.Models;
+using BanHistoryMicroservice.Api.Services;
+using MassTransit;
+using MessageBus.Messages.BanMessages;
+
+namespace BanHistoryMicroservice.Api.MessageBus.MessageBusConsumers
+{
+    public class UserBannedByUserIdConsumer : IConsumer<IUserBannedByUserId>
+    {
+        private readonly IBanService<Ban> banService;
+
+        public UserBannedByUserIdConsumer(IBanService<Ban> banService)
+        {
+           this.banService = banService;
+        }
+        public async Task Consume(ConsumeContext<IUserBannedByUserId> context)
+        {
+            await banService.CreateAsync(new Ban
+            {
+                Id = Guid.NewGuid(), BanType = context.Message.BanType, DurationInDays = context.Message.DurationIdDays,
+                Reason = context.Message.Reason, UserId = context.Message.UserId, UserName = context.Message.UserName
+            });
+        }
+    }
+}
---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/MessageBus/MessageBusConsumers/UserBannedByUserNameConsumer.cs
@@ -0,0 +1,25 @@
+﻿using BanHistoryMicroservice.Api.Models;
+using BanHistoryMicroservice.Api.Services;
+using MassTransit;
+using MessageBus.Messages.BanMessages;
+
+namespace BanHistoryMicroservice.Api.MessageBus.MessageBusConsumers
+{
+    public class UserBannedByUserNameConsumer : IConsumer<IUserBannedByUserName>
+    {
+        private readonly IBanService<Ban> banService;
+
+        public UserBannedByUserNameConsumer(IBanService<Ban> banService)
+        {
+            this.banService = banService;
+        }
+        public async Task Consume(ConsumeContext<IUserBannedByUserName> context)
+        {
+            await banService.CreateAsync(new Ban
+            {
+                Id = Guid.NewGuid(), UserName = context.Message.UserName, Reason = context.Message.Reason,
+                BanType = context.Message.BanType, DurationInDays = context.Message.DurationIdDays, UserId = context.Message.UserId
+            });
+        }
+    }
+}
