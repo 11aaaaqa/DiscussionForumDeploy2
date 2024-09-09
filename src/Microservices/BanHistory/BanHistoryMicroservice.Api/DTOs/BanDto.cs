b6796cd1df341ad078486873140b589fca170989
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 10:39:17 +0400

    Add dto

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/DTOs/BanDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BanHistoryMicroservice.Api.DTOs
+{
+    public class BanDto
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Reason { get; set; }
+        public uint DurationInDays { get; set; }
+        public string BanType { get; set; }
+    }
+}
