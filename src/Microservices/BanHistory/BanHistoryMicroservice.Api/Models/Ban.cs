9ea5721cf77b09c8977ece8880aa6e9b84f4f3cc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 10:09:34 +0400

    Add ban model

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Models/Ban.cs
@@ -0,0 +1,11 @@
+﻿namespace BanHistoryMicroservice.Api.Models
+{
+    public class Ban
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Reason { get; set; }
+        public uint DurationInDays { get; set; }
+        public string BanType { get; set; }
+    }
+}
