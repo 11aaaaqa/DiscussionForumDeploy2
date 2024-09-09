65d03493721e8f082ac91a5f189c059adf7abe6a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 10:25:09 +0400

    Update

--- src/Microservices/BanHistory/BanHistoryMicroservice.Api/Services/IBanService.cs
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Services/IBanService.cs
@@ -3,11 +3,13 @@
     public interface IBanService<TModel>
     {
         Task<List<TModel>> GetAllBansAsync();
-        Task<List<TModel>?> GetByUserIdAsync(Guid userId);
-        Task<List<TModel>?> GetByUserNameAsync(string userName);
-        Task<List<TModel>?> GetByBanTypeAsync(string banType);
+        Task<List<TModel>> GetByUserIdAsync(Guid userId);
+        Task<List<TModel>> GetByUserNameAsync(string userName);
+        Task<List<TModel>> GetByBanTypeAsync(string banType);
         Task<TModel?> GetByIdAsync(Guid id);
         Task<TModel> CreateAsync(TModel model);
         Task DeleteAsync(Guid id);
+        Task DeleteBansByUserId(Guid userId);
+        Task DeleteBansByUserName(string userName);
     }
 }
