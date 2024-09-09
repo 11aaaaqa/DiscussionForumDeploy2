bb5aa0c825ff19f6c5c54cd4d668a8c15147c0e0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 10:11:44 +0400

    Register database context

--- src/Microservices/BanHistory/BanHistoryMicroservice.Api/Program.cs
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Program.cs
@@ -1,15 +1,17 @@
+using BanHistoryMicroservice.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseNpgsql(
+    builder.Configuration["Database:ConnectionString"]));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
