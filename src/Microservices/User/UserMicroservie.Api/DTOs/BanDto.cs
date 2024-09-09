65c04d9b9e93a06946edc270512a7d78059d2db5
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 15:07:32 +0400

    Add dto

---
+++ src/Microservices/User/UserMicroservie.Api/DTOs/BanDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UserMicroservice.Api.DTOs
+{
+    public class BanDto
+    {
+        public string Reason { get; set; }
+        public string BanType { get; set; }
+        public uint ForDays { get; set; }
+    }
+}
