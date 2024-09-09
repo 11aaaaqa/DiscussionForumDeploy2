8419c779d3e1550aee80658bfa330b0416698683
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-09 10:16:10 +0400

    Add user name property

--- src/Microservices/BanHistory/BanHistoryMicroservice.Api/Models/Ban.cs
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Models/Ban.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
+        public string UserName { get; set; }
         public string Reason { get; set; }
         public uint DurationInDays { get; set; }
         public string BanType { get; set; }
