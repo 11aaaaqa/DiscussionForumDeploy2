89bc8af43742a6b2953eda309e4d7497d0736c53
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-10 13:32:22 +0400

    Add migration

--- src/Microservices/Report/ReportMicroservice/Database/Migrations/20240910091854_AddReportedCommentAndDiscussionIdColumns.Designer.cs
+++ src/Microservices/Report/ReportMicroservice/Database/Migrations/20240910091854_AddReportedCommentAndDiscussionIdColumns.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ReportMicroservice.Api.Database;
@@ -11,9 +12,11 @@ using ReportMicroservice.Api.Database;
 namespace ReportMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240910091854_AddReportedCommentAndDiscussionIdColumns")]
+    partial class AddReportedCommentAndDiscussionIdColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Microservices/Report/ReportMicroservice/Database/Migrations/20240910091854_AddReportedCommentAndDiscussionIdColumns.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReportedCommentAndDiscussionIdColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ReportedCommentId",
+                table: "Reports",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ReportedDiscussionId",
+                table: "Reports",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReportedCommentId",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "ReportedDiscussionId",
+                table: "Reports");
+        }
+    }
+}
