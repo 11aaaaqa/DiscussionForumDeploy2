c42aee08a175be546f3c438457dbab1bde343250
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-11 10:37:38 +0400

    Add ban type constants

---
+++ src/Web/Web.MVC/Web.MVC/Constants/BanTypeConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.MVC.Constants
+{
+    public static class BanTypeConstants
+    {
+        public const string GeneralBanType = "Общий";
+        public const string CommentBanType = "Комментарий";
+        public const string DiscussionBanType = "Обсуждение";
+        public const string ReportBanType = "Жалоба";
+    }
+}
