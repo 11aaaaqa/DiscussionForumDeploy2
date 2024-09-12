e300d90b271cc82e7452d604428f8edfd3476125
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-12 17:21:07 +0400

    Update consumer

--- src/Microservices/Report/ReportMicroservice/Services/MessageBusConsumers/DiscussionDeletedConsumer.cs
+++ src/Microservices/Report/ReportMicroservice/Services/MessageBusConsumers/DiscussionDeletedConsumer.cs
@@ -1,19 +1,18 @@
 ﻿using MassTransit;
-using MessageBus.Messages;
+using MessageBus.Messages.BanMessages;
 using Microsoft.EntityFrameworkCore;
 using ReportMicroservice.Api.Database;
 
 namespace ReportMicroservice.Api.Services.MessageBusConsumers
 {
-    public class DiscussionDeletedConsumer : IConsumer<IDiscussionDeleted>
+    public class DiscussionDeletedConsumer : IConsumer<DiscussionDeleted>
     {
         private readonly ApplicationDbContext databaseContext;
-
         public DiscussionDeletedConsumer(ApplicationDbContext databaseContext)
         {
             this.databaseContext = databaseContext;
         }
-        public async Task Consume(ConsumeContext<IDiscussionDeleted> context)
+        public async Task Consume(ConsumeContext<DiscussionDeleted> context)
         {
             var reports =
                 await databaseContext.Reports.Where(x => x.ReportedDiscussionId == context.Message.DiscussionId).ToListAsync();
