72c699708f72bc0b231168e26dab39d9a49980ff
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-12 15:44:19 +0400

    Add message bus message consumer

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Services/MessageBus-Consumers/DiscussionDeletedConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using TopicMicroservice.Api.Models;
+using TopicMicroservice.Api.Services.Repository;
+
+namespace TopicMicroservice.Api.Services.MessageBus_Consumers
+{
+    public class DiscussionDeletedConsumer : IConsumer<IDiscussionDeleted>
+    {
+        private readonly IRepository<Topic> topicRepository;
+
+        public DiscussionDeletedConsumer(IRepository<Topic> topicRepository)
+        {
+            this.topicRepository = topicRepository;
+        }
+        public async Task Consume(ConsumeContext<IDiscussionDeleted> context)
+        {
+            var topic = await topicRepository.GetByNameAsync(context.Message.TopicName);
+            if (topic is not null)
+            {
+                topic.PostsCount -= 1;
+                await topicRepository.UpdateAsync(topic);
+            }
+        }
+    }
+}
