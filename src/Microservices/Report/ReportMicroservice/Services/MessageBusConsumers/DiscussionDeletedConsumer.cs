af553673246782d8d9841232963793d832d668fa
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-12 16:09:30 +0400

    Add discussion deleted message bus message contract consumer

---
+++ src/Microservices/Report/ReportMicroservice/Services/MessageBusConsumers/DiscussionDeletedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using ReportMicroservice.Api.Database;
+
+namespace ReportMicroservice.Api.Services.MessageBusConsumers
+{
+    public class DiscussionDeletedConsumer : IConsumer<IDiscussionDeleted>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public DiscussionDeletedConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IDiscussionDeleted> context)
+        {
+            var reports =
+                await databaseContext.Reports.Where(x => x.ReportedDiscussionId == context.Message.DiscussionId).ToListAsync();
+            foreach (var report in reports)
+            {
+                databaseContext.Reports.Remove(report);
+            }
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
