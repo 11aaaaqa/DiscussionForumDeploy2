22b13d808959703b55cfa142451319c656154812
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-12 16:13:14 +0400

    Add comment deleted message bus message contract

---
+++ src/Microservices/Report/ReportMicroservice/Services/MessageBusConsumers/CommentDeletedConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using ReportMicroservice.Api.Database;
+
+namespace ReportMicroservice.Api.Services.MessageBusConsumers
+{
+    public class CommentDeletedConsumer : IConsumer<ICommentDeleted>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public CommentDeletedConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<ICommentDeleted> context)
+        {
+            var reports = await databaseContext.Reports.Where(x => x.ReportedCommentId == context.Message.CommentId).ToListAsync();
+            foreach (var report in reports)
+            {
+                databaseContext.Reports.Remove(report);
+            }
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
