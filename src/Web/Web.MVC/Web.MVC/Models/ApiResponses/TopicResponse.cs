52fad02bf5c78581dafc222ab43ed2024f42647f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-14 17:29:58 +0400

    Add suggested by property

--- src/Web/Web.MVC/Web.MVC/Models/ApiResponses/TopicResponse.cs
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/TopicResponse.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string SuggestedBy { get; set; }
         public uint PostsCount { get; set; }
     }
 }
