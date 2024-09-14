9c522a54f5efa7a0fd37fc188988b6bec36a6def
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-14 16:13:18 +0400

    Add migration

--- src/Microservices/Topic/TopicMicroservice.Api/Database/Migrations/20240914120209_AddSuggestedByColumn.Designer.cs
+++ src/Microservices/Topic/TopicMicroservice.Api/Database/Migrations/20240914120209_AddSuggestedByColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TopicMicroservice.Api.Database;
@@ -11,9 +12,11 @@ using TopicMicroservice.Api.Database;
 namespace TopicMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240914120209_AddSuggestedByColumn")]
+    partial class AddSuggestedByColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Microservices/Topic/TopicMicroservice.Api/Database/Migrations/20240914120209_AddSuggestedByColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TopicMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSuggestedByColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SuggestedBy",
+                table: "SuggestedTopics",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SuggestedBy",
+                table: "SuggestedTopics");
+        }
+    }
+}
