cab105bf937a996440f760575071699b0c9baef3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-16 17:17:52 +0400

    Add response model

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/CustUserResponses/UserBanInfoResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.MVC.Models.ApiResponses.CustUserResponses
+{
+    public class UserBanInfoResponse
+    {
+        public Guid? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? BanType { get; set; }
+        public string? BanReason { get; set; }
+        public bool IsBanned { get; set; }
+        public DateTime BannedUntil { get; set; }
+    }
+}
