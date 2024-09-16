8f91a492e8caf92b606eea797082952416d07443
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-16 17:16:57 +0400

    Add model

---
+++ src/Microservices/User/UserMicroservie.Api/Models/UserBanInfoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UserMicroservice.Api.Models
+{
+    public class UserBanInfoModel
+    {
+        public Guid? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? BanType { get; set; }
+        public string? BanReason { get; set; }
+        public bool IsBanned { get; set; }
+        public DateTime BannedUntil { get; set; }
+    }
+}
