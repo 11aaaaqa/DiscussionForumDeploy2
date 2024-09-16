5d6ba4a94c3c463358b9aecd5caef55e2fbbeb2b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-16 18:13:15 +0400

    Add user role constants

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Constants/UserRoleConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace RegisterMicroservice.Api.Constants
+{
+    public class UserRoleConstants
+    {
+        public const string AdminRole = "Admin";
+        public const string ModeratorRole = "Moderator";
+        public const string UserRole = "User";
+    }
+}
