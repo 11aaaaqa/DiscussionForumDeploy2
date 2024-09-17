d7f9399118d1f05a742b4a9bf05873942ba7900d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-17 17:22:56 +0400

    Add response model

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/CustUserResponses/UserResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Web.MVC.Models.ApiResponses.CustUserResponses
+{
+    public class UserResponse
+    {
+        public string AspNetUserId { get; set; }
+        public Guid Id { get; set; }
+        public List<Guid> CreatedDiscussionsIds { get; set; }
+        public List<Guid> SuggestedDiscussionsIds { get; set; }
+        public List<Guid> SuggestedCommentsIds { get; set; }
+        public List<Guid> CommentsIds { get; set; }
+        public string UserName { get; set; }
+        public uint Posts { get; set; }
+        public uint Answers { get; set; }
+        public DateOnly RegisteredAt { get; set; }
+        public bool IsBanned { get; set; }
+        public DateTime BannedUntil { get; set; }
+        public string BannedFor { get; set; }
+        public string BanType { get; set; }
+    }
+}
