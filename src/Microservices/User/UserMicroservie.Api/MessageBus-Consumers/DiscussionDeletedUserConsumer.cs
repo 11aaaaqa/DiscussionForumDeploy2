2c5922655e862000393385af8b7aff53b1608c2f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-18 18:55:25 +0400

    Add message bus message consumer

---
+++ src/Microservices/User/UserMicroservie.Api/MessageBus-Consumers/DiscussionDeletedUserConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.MessageBus_Consumers
+{
+    public class DiscussionDeletedUserConsumer : IConsumer<IDiscussionDeleted>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public DiscussionDeletedUserConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IDiscussionDeleted> context)
+        {
+            var user = await databaseContext.Users.SingleOrDefaultAsync(x =>
+                x.UserName == context.Message.UserNameDiscussionCreatedBy);
+            if (user is not null)
+            {
+                user.Posts -= 1;
+                databaseContext.Users.Update(user);
+                await databaseContext.SaveChangesAsync();
+            }
+        }
+    }
+}
