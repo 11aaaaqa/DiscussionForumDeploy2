b410fb5d359270b2ecb099249096bddecb94cdcd
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-19 16:15:54 +0400

    Add suggest discussion controller

---
+++ src/Web/Web.MVC/Web.MVC/Controllers/SuggestDiscussion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.MVC.Controllers
+{
+    public class SuggestDiscussion : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public SuggestDiscussion(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteSuggestedDiscussion(Guid suggestedDiscussionId, string returnUrl)
+        {
+            using var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.DeleteAsync(
+                $"http://discussion-microservice-api:8080/api/SuggestDiscussion/RejectSuggestedDiscussion/{suggestedDiscussionId}");
+            if (!response.IsSuccessStatusCode) return View("ActionError");
+
+            if (!string.IsNullOrEmpty(returnUrl))
+                return LocalRedirect(returnUrl);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
