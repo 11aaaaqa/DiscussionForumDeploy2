ce4930b57525379d725746b4da56f70f059b10f1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-19 16:15:47 +0400

    Add suggest comment controller

---
+++ src/Web/Web.MVC/Web.MVC/Controllers/SuggestComment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.MVC.Controllers
+{
+    public class SuggestComment : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public SuggestComment(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteSuggestedComment(Guid suggestedCommentId, string returnUrl)
+        {
+            using var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.DeleteAsync(
+                $"http://comment-microservice-api:8080/api/SuggestComment/RejectSuggestedComment/{suggestedCommentId}");
+            if (!response.IsSuccessStatusCode) return View("ActionError");
+
+            if (!string.IsNullOrEmpty(returnUrl))
+                return LocalRedirect(returnUrl);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
