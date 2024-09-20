2e2efe8a5416c600f4a7017408ef038a7cb64a98
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-20 20:08:25 +0400

    Add constants

---
+++ src/Web/Web.MVC/Web.MVC/Constants/UserRoleConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.MVC.Constants
+{
+    public class UserRoleConstants
+    {
+        public const string AdminRole = "Admin";
+        public const string ModeratorRole = "Moderator";
+        public const string UserRole = "User";
+    }
+}
