3b4dfa807bc95389dc628726811870c1619aba29
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-21 19:53:28 +0400

    Add controller

---
+++ src/Web/Web.MVC/Web.MVC/Controllers/SuggestTopic.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.MVC.Controllers
+{
+    public class SuggestTopic : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public SuggestTopic(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteSuggestedTopicById(Guid id, string returnUrl)
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.DeleteAsync(
+                $"http://topic-microservice-api:8080/api/SuggestTopic/RejectSuggestedTopic/{id}");
+            if (!response.IsSuccessStatusCode) return View("ActionError");
+
+            if (!string.IsNullOrEmpty(returnUrl)) 
+                return LocalRedirect(returnUrl);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
