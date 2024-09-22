c953d7e1bac3fe74ccc37dfe9bd772a1de2e09f7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-22 11:06:02 +0400

    Add dto

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/DTOs/DecreaseDiscussionRatingByOneMethodDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscussionMicroservice.Api.DTOs
+{
+    public class DecreaseDiscussionRatingByOneMethodDto
+    {
+        public string UserNameDecreasedBy { get; set; }
+        public Guid DiscussionId { get; set; }
+    }
+}
