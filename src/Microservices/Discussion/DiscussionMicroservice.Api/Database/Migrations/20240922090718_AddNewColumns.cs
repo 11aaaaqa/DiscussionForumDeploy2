1e77277b2f2e9dd1ba0c170ed3fd4019dbb3f62b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-22 13:08:06 +0400

    Add migration

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Database/Migrations/20240922090718_AddNewColumns.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscussionMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "UsersDecreasedRating",
+                table: "Discussions",
+                type: "text[]",
+                nullable: false);
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "UsersIncreasedRating",
+                table: "Discussions",
+                type: "text[]",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsersDecreasedRating",
+                table: "Discussions");
+
+            migrationBuilder.DropColumn(
+                name: "UsersIncreasedRating",
+                table: "Discussions");
+        }
+    }
+}
