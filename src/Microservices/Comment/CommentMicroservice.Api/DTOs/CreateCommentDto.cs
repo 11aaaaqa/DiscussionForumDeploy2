97f93f8462f51fc8a8e03938ff51d0e1e687bd96
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-23 17:22:27 +0400

    Add dto

---
+++ src/Microservices/Comment/CommentMicroservice.Api/DTOs/CreateCommentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CommentMicroservice.Api.DTOs
+{
+    public class CreateCommentDto
+    {
+        public Guid DiscussionId { get; set; }
+        public string CreatedBy { get; set; }
+        public string Content { get; set; }
+    }
+}
