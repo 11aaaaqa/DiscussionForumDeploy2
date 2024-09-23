bd5689c6e9d4b8ee599dad70a913ffc9340efe83
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-23 16:56:52 +0400

    Add dto

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/DTOs/CreateDiscussionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscussionMicroservice.Api.DTOs
+{
+    public class CreateDiscussionDto
+    {
+        public string TopicName { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
