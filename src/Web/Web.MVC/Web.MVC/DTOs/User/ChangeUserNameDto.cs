076dcc5a8f4a6b5e92291425fc88109ed171da16
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-24 18:20:50 +0400

    Add dto

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/User/ChangeUserNameDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.User
+{
+    public class ChangeUserNameDto
+    {
+        [Required(ErrorMessage = "Поле \"Новое имя пользователя\" обязательно")]
+        [StringLength(30, ErrorMessage = "Максимальная длина имени пользователя - 30 символов")]
+        [Display(Name = "Новое имя пользователя")]
+        public string NewUserName { get; set; }
+    }
+}
