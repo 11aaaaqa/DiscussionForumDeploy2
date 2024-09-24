151bb98c2f47128e45aab588f80148f3a54cbfbd
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-24 17:47:34 +0400

    Add message consumer

---
+++ src/Microservices/Report/ReportMicroservice/MessageBus/Consumers/UserNameChangedReportConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using ReportMicroservice.Api.Database;
+
+namespace ReportMicroservice.Api.MessageBus.Consumers
+{
+    public class UserNameChangedReportConsumer : IConsumer<IUserNameChanged>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserNameChangedReportConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserNameChanged> context)
+        {
+            var reportsWithOldUserName = await databaseContext.Reports.Where(x => x.UserNameReportedBy == context.Message.OldUserName)
+                .ToListAsync();
+            foreach (var reportWithOldUserName in reportsWithOldUserName)
+            {
+                reportWithOldUserName.UserNameReportedBy = context.Message.NewUserName;
+            }
+
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
