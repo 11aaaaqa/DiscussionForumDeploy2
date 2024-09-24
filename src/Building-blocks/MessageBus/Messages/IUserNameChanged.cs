742e2e5bcc034aa75f968f2d65add4ff505edf65
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-24 17:03:12 +0400

    Add message bus message contract

---
+++ src/Building-blocks/MessageBus/Messages/IUserNameChanged.cs
@@ -0,0 +1,8 @@
+﻿namespace MessageBus.Messages
+{
+    public interface IUserNameChanged
+    {
+        public string OldUserName { get; set; }
+        public string NewUserName { get; set; }
+    }
+}
