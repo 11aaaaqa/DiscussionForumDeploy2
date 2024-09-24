297e2543f1e5a9b67457946f8289f9021b7377a1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-24 17:23:38 +0400

    Add message bus message consumer

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/MessageBus/MessageBusConsumers/UserNameChangedBanHistoryConsumer.cs
@@ -0,0 +1,28 @@
+﻿using BanHistoryMicroservice.Api.Database;
+using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanHistoryMicroservice.Api.MessageBus.MessageBusConsumers
+{
+    public class UserNameChangedBanHistoryConsumer : IConsumer<IUserNameChanged>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserNameChangedBanHistoryConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserNameChanged> context)
+        {
+            var bansWithOldUserName = await databaseContext.Bans.Where(x => x.UserName == context.Message.OldUserName)
+                .ToListAsync();
+            foreach (var banWithOldUserName in bansWithOldUserName)
+            {
+                banWithOldUserName.UserName = context.Message.NewUserName;
+            }
+
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
