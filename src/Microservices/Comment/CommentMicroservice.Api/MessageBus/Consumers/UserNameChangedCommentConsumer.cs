324be4c463b74e27f78ccb2803f29939cf3e5ef1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-24 17:33:21 +0400

    Add message consumer

---
+++ src/Microservices/Comment/CommentMicroservice.Api/MessageBus/Consumers/UserNameChangedCommentConsumer.cs
@@ -0,0 +1,35 @@
+﻿using CommentMicroservice.Api.Database;
+using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentMicroservice.Api.MessageBus.Consumers
+{
+    public class UserNameChangedCommentConsumer : IConsumer<IUserNameChanged>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserNameChangedCommentConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserNameChanged> context)
+        {
+            var commentsWithOldUserName = await databaseContext.Comments
+                .Where(x => x.CreatedBy == context.Message.OldUserName).ToListAsync();
+            foreach (var commentWithOldUserName in commentsWithOldUserName)
+            {
+                commentWithOldUserName.CreatedBy = context.Message.NewUserName;
+            }
+
+            var suggestedCommentsWithOldUserName = await databaseContext.SuggestedComments
+                .Where(x => x.CreatedBy == context.Message.OldUserName).ToListAsync();
+            foreach (var suggestedCommentWithOldUserName in suggestedCommentsWithOldUserName)
+            {
+                suggestedCommentWithOldUserName.CreatedBy = context.Message.NewUserName;
+            }
+
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
