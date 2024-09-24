27a91e99dc8596f7280f4311b653167228e933f7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-24 17:50:18 +0400

    Add consumer

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Services/MessageBus-Consumers/UserNameChangedTopicConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using TopicMicroservice.Api.Database;
+
+namespace TopicMicroservice.Api.Services.MessageBus_Consumers
+{
+    public class UserNameChangedTopicConsumer : IConsumer<IUserNameChanged>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserNameChangedTopicConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserNameChanged> context)
+        {
+            var suggestedTopicsWithOldUserName = await databaseContext.SuggestedTopics.
+                Where(x => x.SuggestedBy == context.Message.OldUserName).ToListAsync();
+            foreach (var suggestedTopicWithOldUserName in suggestedTopicsWithOldUserName)
+            {
+                suggestedTopicWithOldUserName.SuggestedBy = context.Message.NewUserName;
+            }
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
