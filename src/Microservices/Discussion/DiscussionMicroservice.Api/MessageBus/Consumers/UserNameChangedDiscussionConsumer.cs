dd0c580a8378329e2390fa67854a94f66bee9868
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-24 17:39:11 +0400

    Add message consumer

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/MessageBus/Consumers/UserNameChangedDiscussionConsumer.cs
@@ -0,0 +1,35 @@
+﻿using DiscussionMicroservice.Api.Database;
+using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscussionMicroservice.Api.MessageBus.Consumers
+{
+    public class UserNameChangedDiscussionConsumer : IConsumer<IUserNameChanged>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserNameChangedDiscussionConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserNameChanged> context)
+        {
+            var discussionsWithOldCreatedByUserName = await databaseContext.Discussions
+                .Where(x => x.CreatedBy == context.Message.OldUserName).ToListAsync();
+            foreach (var discussionWithOldCreatedByUserName in discussionsWithOldCreatedByUserName)
+            {
+                discussionWithOldCreatedByUserName.CreatedBy = context.Message.NewUserName;
+            }
+
+            var suggestedDiscussionsWithOldCreatedByUserName = await databaseContext.SuggestedDiscussions
+                .Where(x => x.CreatedBy == context.Message.OldUserName).ToListAsync();
+            foreach (var suggestedDiscussionWithOldCreatedByUserName in suggestedDiscussionsWithOldCreatedByUserName)
+            {
+                suggestedDiscussionWithOldCreatedByUserName.CreatedBy = context.Message.NewUserName;
+            }
+
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
