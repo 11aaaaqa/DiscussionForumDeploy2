3001c7347f86e966c14efe100b7b1b17f6ead2bd
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-24 17:44:05 +0400

    Add message consumer

---
+++ src/Microservices/Register/RegisterMicroservice.Api/MessageBus/Consumers/UserNameChangedRegisterConsumer.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using RegisterMicroservice.Api.Database;
+
+namespace RegisterMicroservice.Api.MessageBus.Consumers
+{
+    public class UserNameChangedRegisterConsumer : IConsumer<IUserNameChanged>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserNameChangedRegisterConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserNameChanged> context)
+        {
+            var userWithOldUserName = await databaseContext.Users.SingleAsync(x => x.UserName == context.Message.OldUserName);
+            userWithOldUserName.UserName = context.Message.NewUserName;
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
