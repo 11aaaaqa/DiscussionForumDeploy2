9ee61476007c88372c9105fdd1ff13e2b4432f33
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-26 15:39:26 +0400

    Add dto

---
+++ src/Microservices/Register/RegisterMicroservice.Api/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RegisterMicroservice.Api.DTOs
+{
+    public class ChangePasswordDto
+    {
+        public Guid UserId { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
