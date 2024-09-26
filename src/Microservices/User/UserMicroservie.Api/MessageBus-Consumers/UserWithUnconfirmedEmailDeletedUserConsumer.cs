64ae58e43c9e8ef2a28bff2c38be219b1eda6147
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-26 19:15:29 +0400

    Add message consumer

---
+++ src/Microservices/User/UserMicroservie.Api/MessageBus-Consumers/UserWithUnconfirmedEmailDeletedUserConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Api.Database;
+
+namespace UserMicroservice.Api.MessageBus_Consumers
+{
+    public class UserWithUnconfirmedEmailDeletedUserConsumer : IConsumer<IUserWithUnconfirmedEmailDeleted>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public UserWithUnconfirmedEmailDeletedUserConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext  = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<IUserWithUnconfirmedEmailDeleted> context)
+        {
+            var user = await databaseContext.Users.SingleOrDefaultAsync(x =>
+                x.AspNetUserId == context.Message.AspNetUserId);
+            if (user is not null)
+            {
+                databaseContext.Users.Remove(user);
+                await databaseContext.SaveChangesAsync();
+            }
+        }
+    }
+}
