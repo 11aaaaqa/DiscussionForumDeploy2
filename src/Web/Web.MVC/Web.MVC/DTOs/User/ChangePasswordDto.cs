0d10de28bca6a4e7ba37c55484934434a4b32b65
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-26 15:46:42 +0400

    Add dto

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/User/ChangePasswordDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.User
+{
+    public class ChangePasswordDto
+    {
+        public Guid? UserId { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Старый пароль\" обязательно")]
+        [StringLength(40)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Старый пароль")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Новый пароль\" обязательно")]
+        [StringLength(40, ErrorMessage = "Пароль должен содержать как минимум 8 символов", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        public string NewPassword { get; set; }
+    }
+}
