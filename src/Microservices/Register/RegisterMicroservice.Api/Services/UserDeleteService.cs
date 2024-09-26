58fb7e354bce256cad46f07bd2e0b1eaf02aae07
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-26 19:09:45 +0400

    Add service

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Services/UserDeleteService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RegisterMicroservice.Api.Models.UserModels;
+
+namespace RegisterMicroservice.Api.Services
+{
+    public class UserDeleteService
+    {
+        private readonly UserManager<User> userManager;
+
+        public UserDeleteService(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public void DeleteUnconfirmedUser(string userId)
+        {
+            var user = userManager.Users.Single(x => x.Id == userId);
+            userManager.DeleteAsync(user);
+        }
+    }
+}
