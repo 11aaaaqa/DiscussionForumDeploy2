fd7dad9bb397fe5a128a1512186568990d2493f8
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-09-28 12:59:17 +0400

    Add dto

---
+++ src/Web/Web.MVC/Web.MVC/DTOs/Moderator/CheckBoxUserRoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.DTOs.Moderator
+{
+    public class CheckBoxUserRoleDto
+    {
+        public bool IsChecked { get; set; }
+        public string RoleName { get; set; }
+    }
+}
