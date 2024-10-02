fed065e190f3e295929efc0a116324528a369a34
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-02 15:33:28 +0400

    Add constant

---
+++ src/Web/Web.MVC/Web.MVC/Constants/UserSearchingTypeConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Constants
+{
+    public class UserSearchingTypeConstants
+    {
+        public const string UserNameSearchingType = "UserName";
+        public const string EmailSearchingType = "Email";
+    }
+}
