d7515c32353f4b63e573d9f2ddb953f9c63d3b6b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-02 16:28:01 +0400

    Add constants

---
+++ src/Web/Web.MVC/Web.MVC/Constants/SearchingSortTypeConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Constants
+{
+    public class SearchingSortTypeConstants
+    {
+        public const string Ascending = "По возрастанию";
+        public const string Descending = "По убыванию";
+    }
+}
