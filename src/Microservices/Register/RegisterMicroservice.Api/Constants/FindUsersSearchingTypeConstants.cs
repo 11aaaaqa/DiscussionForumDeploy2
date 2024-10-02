6580b712d2675dfd37c1617dcaa9328937002f43
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-02 15:49:50 +0400

    Add constants

---
+++ src/Microservices/Register/RegisterMicroservice.Api/Constants/FindUsersSearchingTypeConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace RegisterMicroservice.Api.Constants
+{
+    public class FindUsersSearchingTypeConstants
+    {
+        public const string UserNameSearchingType = "Имя пользователя";
+        public const string EmailSearchingType = "Почта";
+    }
+}
