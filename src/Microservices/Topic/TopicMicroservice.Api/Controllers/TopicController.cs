62f96ab82b7472f2debf6e41a54477e3614a1229
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-02 18:49:37 +0400

    Refactoring

--- src/Microservices/Topic/TopicMicroservice.Api/Controllers/TopicController.cs
+++ src/Microservices/Topic/TopicMicroservice.Api/Controllers/TopicController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TopicMicroservice.Api.DTOs;
 using TopicMicroservice.Api.Models;
 using TopicMicroservice.Api.Services.Repository;
