822971f45f41eabaa552bfce100f3adaf2667164
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-03 17:09:36 +0400

    Add method overload

--- src/Microservices/Topic/TopicMicroservice.Api/Services/ITopicService.cs
+++ src/Microservices/Topic/TopicMicroservice.Api/Services/ITopicService.cs
@@ -3,5 +3,6 @@
     public interface ITopicService
     {
         Task<bool> DoesAllTopicsHaveNextPage(int pageSize, int pageNumber);
+        Task<bool> DoesAllTopicsHaveNextPage(int pageSize, int pageNumber, string searchingQuery);
     }
 }
--- src/Microservices/Topic/TopicMicroservice.Api/Services/TopicService.cs
+++ src/Microservices/Topic/TopicMicroservice.Api/Services/TopicService.cs
@@ -18,5 +18,13 @@ namespace TopicMicroservice.Api.Services
             int startedOnCurrentPageTopicsCount = totalRequestedTopicsCount - pageSize;
             return (totalTopicsCount > startedOnCurrentPageTopicsCount);
         }
+
+        public async Task<bool> DoesAllTopicsHaveNextPage(int pageSize, int pageNumber, string searchingQuery)
+        {
+            int totalTopicsCount = await context.Topics.Where(x => x.Name.Contains(searchingQuery)).CountAsync();
+            int totalRequestedTopicsCount = pageNumber * pageSize;
+            int startedOnCurrentPageTopicsCount = totalRequestedTopicsCount - pageSize;
+            return (totalTopicsCount > startedOnCurrentPageTopicsCount);
+        }
     }
 }
