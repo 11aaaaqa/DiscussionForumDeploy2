fe7fce5b5818e1577072507704f150ce1e8e6361
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-04 15:11:33 +0400

    Add created at property

--- src/Microservices/Topic/TopicMicroservice.Api/Models/Topic.cs
+++ src/Microservices/Topic/TopicMicroservice.Api/Models/Topic.cs
@@ -8,5 +8,6 @@ namespace TopicMicroservice.Api.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public  uint PostsCount { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
