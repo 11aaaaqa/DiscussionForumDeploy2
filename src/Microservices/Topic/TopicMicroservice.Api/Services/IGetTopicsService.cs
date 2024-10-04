19e80bc9756da99305de65f41076c1f6c2dbd9f3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-04 15:16:31 +0400

    Add service interface

---
+++ src/Microservices/Topic/TopicMicroservice.Api/Services/IGetTopicsService.cs
@@ -0,0 +1,10 @@
+﻿using TopicMicroservice.Api.Models;
+
+namespace TopicMicroservice.Api.Services
+{
+    public interface IGetTopicsService
+    {
+        Task<List<Topic>> GetAllTopicSortedByNovelty(TopicParameters topicParameters);
+        Task<List<Topic>> GetAllTopicSortedByPopularity(TopicParameters topicParameters);
+    }
+}
