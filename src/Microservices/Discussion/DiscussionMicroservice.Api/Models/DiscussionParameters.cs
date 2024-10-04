d9ce03a34027505889efc2c45155bacfb426e4b8
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-04 16:41:41 +0400

    Add pagination model

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Models/DiscussionParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace DiscussionMicroservice.Api.Models
+{
+    public class DiscussionParameters
+    {
+        private const int MaxPageSize = 30;
+        public int PageNumber { get; set; } = 1;
+
+        private int pageSize = 10;
+
+        public int PageSize
+        {
+            get => pageSize;
+            set => pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
