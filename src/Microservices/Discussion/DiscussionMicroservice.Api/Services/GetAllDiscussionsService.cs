a48953f6e28d2aac183ef24ce7cc3abc7a2290d9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-05 18:47:47 +0400

    Add getting sorted discussions by topic name service

---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Services/GetAllDiscussionsService.cs
@@ -0,0 +1,33 @@
+﻿using DiscussionMicroservice.Api.Database;
+using DiscussionMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscussionMicroservice.Api.Services
+{
+    public class GetAllDiscussionsService : IGetAllDiscussionsService
+    {
+        private readonly ApplicationDbContext context;
+
+        public GetAllDiscussionsService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<Discussion>> GetAllDiscussionsSortedByNovelty(DiscussionParameters discussionParameters, string topicName)
+        {
+            var discussions = await context.Discussions.Where(x => x.TopicName == topicName)
+                .OrderByDescending(x => x.CreatedAt)
+                .Skip(discussionParameters.PageSize * (discussionParameters.PageNumber - 1))
+                .Take(discussionParameters.PageSize).ToListAsync();
+            return discussions;
+        }
+
+        public async Task<List<Discussion>> GetAllDiscussionsSortedByPopularity(DiscussionParameters discussionParameters, string topicName)
+        {
+            var discussions = await context.Discussions.Where(x => x.TopicName == topicName)
+                .OrderByDescending(x => x.Rating)
+                .Skip(discussionParameters.PageSize * (discussionParameters.PageNumber - 1))
+                .Take(discussionParameters.PageSize).ToListAsync();
+            return discussions;
+        }
+    }
+}
---
+++ src/Microservices/Discussion/DiscussionMicroservice.Api/Services/IGetAllDiscussionsService.cs
@@ -0,0 +1,10 @@
+﻿using DiscussionMicroservice.Api.Models;
+
+namespace DiscussionMicroservice.Api.Services
+{
+    public interface IGetAllDiscussionsService
+    {
+        Task<List<Discussion>> GetAllDiscussionsSortedByNovelty(DiscussionParameters discussionParameters, string topicName);
+        Task<List<Discussion>> GetAllDiscussionsSortedByPopularity(DiscussionParameters discussionParameters, string topicName);
+    }
+}
