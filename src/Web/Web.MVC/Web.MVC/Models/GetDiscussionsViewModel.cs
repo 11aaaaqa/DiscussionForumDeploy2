087657cedb67b60c895727b632b4735d84489607
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-06 14:26:08 +0400

    Add view model

---
+++ src/Web/Web.MVC/Web.MVC/Models/GetDiscussionsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Web.MVC.Models.ApiResponses;
+
+namespace Web.MVC.Models
+{
+    public class GetDiscussionsViewModel
+    {
+        public List<DiscussionResponse> Discussions { get; set; }
+        public int PageSize { get; set; }
+        public bool DoesNextPageExist { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int NextPageNumber { get; set; }
+        public int PreviousPageNumber { get; set; }
+    }
+}
