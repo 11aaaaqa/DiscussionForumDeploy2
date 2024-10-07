00f6e826ed0429f7f05806d0d0adc76339dbcfa9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-07 16:20:26 +0400

    Add pagination model

---
+++ src/Microservices/Report/ReportMicroservice/Models/ReportParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace ReportMicroservice.Api.Models
+{
+    public class ReportParameters
+    {
+        private const int MaxPageSize = 30;
+        public int PageNumber { get; set; } = 1;
+
+        private int pageSize = 10;
+
+        public int PageSize
+        {
+            get => pageSize;
+            set => pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
