d6756a62e6088b0726bf7db303b696a3cafcba61
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-08 16:45:05 +0400

    Add model

---
+++ src/Web/Web.MVC/Web.MVC/Models/GetReportsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Web.MVC.Models.ApiResponses.ReportResponses;
+
+namespace Web.MVC.Models
+{
+    public class GetReportsViewModel
+    {
+        public List<ReportApiResponse> Reports { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int NextPageNumber { get; set; }
+        public int PreviousPageNumber { get; set; }
+        public bool DoesNextPageExist { get; set; }
+        public string? ReportType { get; set; }
+    }
+}
