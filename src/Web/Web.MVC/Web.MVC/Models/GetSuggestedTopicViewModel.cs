6585bcac6f6a18067bab42daa567e01aa8db521c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-09 16:36:42 +0400

    Add model

---
+++ src/Web/Web.MVC/Web.MVC/Models/GetSuggestedTopicViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Web.MVC.Models.ApiResponses;
+
+namespace Web.MVC.Models
+{
+    public class GetSuggestedTopicViewModel
+    {
+        public List<TopicResponse> SuggestedTopics { get; set; }
+        public bool DoesNextPageExist { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int NextPageNumber { get; set; }
+        public int PreviousPageNumber { get; set;}
+    }
+}
