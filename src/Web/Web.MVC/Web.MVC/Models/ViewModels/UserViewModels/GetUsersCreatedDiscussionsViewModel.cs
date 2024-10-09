b624a37ee3c8d9832a65e9bfefee8d79ffcb36e6
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-09 17:13:31 +0400

    Add view model

---
+++ src/Web/Web.MVC/Web.MVC/Models/ViewModels/UserViewModels/GetUsersCreatedDiscussionsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Web.MVC.Models.ApiResponses.Discussion;
+
+namespace Web.MVC.Models.ViewModels.UserViewModels
+{
+    public class GetUsersCreatedDiscussionsViewModel
+    {
+        public List<DiscussionResponse> CreatedDiscussions { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int PreviousPageNumber { get; set; }
+        public int NextNumber { get; set; }
+        public bool DoesNextPageExist { get; set; }
+    }
+}
