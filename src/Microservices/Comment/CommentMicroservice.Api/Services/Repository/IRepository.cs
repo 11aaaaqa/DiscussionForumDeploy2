417bc75a5606aaacd8b88f7866cf5071f70ce190
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-10 15:28:33 +0400

    Add pagination in getting all comments by discussions id method

--- src/Microservices/Comment/CommentMicroservice.Api/Services/Repository/IRepository.cs
+++ src/Microservices/Comment/CommentMicroservice.Api/Services/Repository/IRepository.cs
@@ -5,7 +5,7 @@ namespace CommentMicroservice.Api.Services.Repository
     public interface IRepository<TModel>
     {
         Task<List<TModel>> GetAllAsync(CommentParameters commentParameters);
-        Task<List<TModel>?> GetByDiscussionIdAsync(Guid id);
+        Task<List<TModel>> GetByDiscussionIdAsync(Guid id, CommentParameters commentParameters);
         Task<List<TModel>> GetByIds(params Guid[] ids);
         Task<List<TModel>> GetByUserName(string userName);
         Task<TModel?> GetByIdAsync(Guid id);
