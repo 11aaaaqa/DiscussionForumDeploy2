f210531900f21450f2cc064d79c9a6bbcaecf254
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-10 14:54:09 +0400

    Add pagination service

---
+++ src/Microservices/Comment/CommentMicroservice.Api/Services/IPaginationService.cs
@@ -0,0 +1,10 @@
+﻿using CommentMicroservice.Api.Models;
+
+namespace CommentMicroservice.Api.Services
+{
+    public interface IPaginationService
+    {
+        Task<bool> DoesNextCommentsPageExistAsync(CommentParameters commentParameters);
+        Task<bool> DoesNextSuggestedCommentsPageExistAsync(CommentParameters commentParameters);
+    }
+}
---
+++ src/Microservices/Comment/CommentMicroservice.Api/Services/PaginationService.cs
@@ -0,0 +1,31 @@
+﻿using CommentMicroservice.Api.Database;
+using CommentMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentMicroservice.Api.Services
+{
+    public class PaginationService : IPaginationService
+    {
+        private readonly ApplicationDbContext context;
+
+        public PaginationService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<bool> DoesNextCommentsPageExistAsync(CommentParameters commentParameters)
+        {
+            int totalCommentsCount = await context.Comments.CountAsync();
+            int totalRequestedCommentsCount = commentParameters.PageSize * commentParameters.PageNumber;
+            int startRequestedCommentsCount = totalRequestedCommentsCount - commentParameters.PageSize;
+            return (totalCommentsCount > startRequestedCommentsCount);
+        }
+
+        public async Task<bool> DoesNextSuggestedCommentsPageExistAsync(CommentParameters commentParameters)
+        {
+            int totalSuggestedCommentsCount = await context.SuggestedComments.CountAsync();
+            int totalRequestedSuggestedCommentsCount = commentParameters.PageSize * commentParameters.PageNumber;
+            int startRequestedSuggestedCommentsCount = totalRequestedSuggestedCommentsCount - commentParameters.PageSize;
+            return (totalSuggestedCommentsCount > startRequestedSuggestedCommentsCount);
+        }
+    }
+}
