61c0525e6a2788b767c7d25ac4cfd81d2a92a49e
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-11 18:58:01 +0400

    Add model

---
+++ src/Web/Web.MVC/Web.MVC/Models/ViewModels/UserViewModels/SuggestedCommentsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Web.MVC.Models.ApiResponses.CommentsResponses;
+
+namespace Web.MVC.Models.ViewModels.UserViewModels
+{
+    public class SuggestedCommentsViewModel
+    {
+        public List<SuggestedCommentResponse> SuggestedComments { get; set; }
+        public int PageSize { get; set; }
+        public bool DoesNextPageExist { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int NextPageNumber { get; set; }
+        public int PreviousPageNumber { get; set; }
+    }
+}
