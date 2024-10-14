ac90bfc3cb4f2fe0049c78a5f22ba6e779fe5928
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-14 16:56:18 +0400

    Add pagination model

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Models/BanHistoryParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace BanHistoryMicroservice.Api.Models
+{
+    public class BanHistoryParameters
+    {
+        private const int MaxPageSize = 30;
+        public int PageNumber { get; set; } = 1;
+
+        private int pageSize = 15;
+        public int PageSize
+        {
+            get => pageSize;
+            set => pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
