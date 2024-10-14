14b1d2e3fd1a2c390711afab448cdbe65dd45a20
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-14 16:26:10 +0400

    Add migration

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Database/Migrations/20241014121504_AddBannedByColumnInBanHistoryTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanHistoryMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBannedByColumnInBanHistoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BannedBy",
+                table: "Bans",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BannedBy",
+                table: "Bans");
+        }
+    }
+}
