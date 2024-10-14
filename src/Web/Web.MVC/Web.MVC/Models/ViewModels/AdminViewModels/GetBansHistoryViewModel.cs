66487e5fb0582d0700250deff8a67828e9245fd1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-14 19:16:13 +0400

    Update model by adding page size

--- src/Web/Web.MVC/Web.MVC/Models/ViewModels/AdminViewModels/GetBansHistoryViewModel.cs
+++ src/Web/Web.MVC/Web.MVC/Models/ViewModels/AdminViewModels/GetBansHistoryViewModel.cs
@@ -6,6 +6,7 @@ namespace Web.MVC.Models.ViewModels.AdminViewModels
     {
         public List<BanHistoryModel> Bans { get; set; }
         public bool DoesNextPageExist { get; set; }
+        public int PageSize { get; set; }
         public int CurrentPageNumber { get; set; }
         public int NextPageNumber { get; set; }
         public int PreviousPageNumber { get; set; }
