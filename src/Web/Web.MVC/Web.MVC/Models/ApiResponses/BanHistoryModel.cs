6a6ff7ad15e8c8059df011b5e27b09d677401303
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-14 19:03:07 +0400

    Add models

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/BanHistoryModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.MVC.Models.ApiResponses
+{
+    public class BanHistoryModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Reason { get; set; }
+        public uint DurationInDays { get; set; }
+        public string BanType { get; set; }
+        public string BannedBy { get; set; }
+        public DateTime BannedFrom { get; set; }
+        public DateTime BannedUntil { get; set; }
+    }
+}
---
+++ src/Web/Web.MVC/Web.MVC/Models/ViewModels/AdminViewModels/GetBansHistoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Web.MVC.Models.ApiResponses;
+
+namespace Web.MVC.Models.ViewModels.AdminViewModels
+{
+    public class GetBansHistoryViewModel
+    {
+        public List<BanHistoryModel> Bans { get; set; }
+        public bool DoesNextPageExist { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int NextPageNumber { get; set; }
+        public int PreviousPageNumber { get; set; }
+        public string? SearchingQuery { get; set; }
+    }
+}
