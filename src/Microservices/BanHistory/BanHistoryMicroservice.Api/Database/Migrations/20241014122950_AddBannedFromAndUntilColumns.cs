9078f88ee91d2545dcab8d10734d30ede400d38b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-14 16:40:37 +0400

    Add migration

---
+++ src/Microservices/BanHistory/BanHistoryMicroservice.Api/Database/Migrations/20241014122950_AddBannedFromAndUntilColumns.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanHistoryMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBannedFromAndUntilColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BannedFrom",
+                table: "Bans",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BannedUntil",
+                table: "Bans",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BannedFrom",
+                table: "Bans");
+
+            migrationBuilder.DropColumn(
+                name: "BannedUntil",
+                table: "Bans");
+        }
+    }
+}
