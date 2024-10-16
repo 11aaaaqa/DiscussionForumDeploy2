ba5dd3261dcb69cf49be8f9807ec26b7f6eeb6a5
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-16 18:31:44 +0400

    Register message bus message consumer

--- src/Microservices/Comment/CommentMicroservice.Api/Program.cs
+++ src/Microservices/Comment/CommentMicroservice.Api/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMassTransit(x =>
 {
     x.SetKebabCaseEndpointNameFormatter();
+    x.AddConsumer<CommentDeletedCommentConsumer>();
     x.AddConsumer<UserNameChangedCommentConsumer>();
     x.UsingRabbitMq((context, config) =>
     {
