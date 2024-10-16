b7435c593f02c77e61acb8a204f8503fe92bd927
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-16 18:29:46 +0400

    Update dtos by adding replies properties

--- src/Microservices/Comment/CommentMicroservice.Api/DTOs/CreateCommentDto.cs
+++ src/Microservices/Comment/CommentMicroservice.Api/DTOs/CreateCommentDto.cs
@@ -5,5 +5,8 @@
         public Guid DiscussionId { get; set; }
         public string CreatedBy { get; set; }
         public string Content { get; set; }
+        public Guid? RepliedOnCommentId { get; set; }
+        public string? RepliedOnCommentCreatedBy { get; set; }
+        public string? RepliedOnCommentContent { get; set; }
     }
 }
