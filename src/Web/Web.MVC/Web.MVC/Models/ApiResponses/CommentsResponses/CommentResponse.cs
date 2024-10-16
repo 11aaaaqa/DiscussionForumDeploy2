d7ba429f68ec90c7820a0bfc83750238985da682
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-16 18:29:24 +0400

    Update models by adding replies properties

--- src/Web/Web.MVC/Web.MVC/Models/ApiResponses/CommentsResponses/CommentResponse.cs
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/CommentsResponses/CommentResponse.cs
@@ -7,5 +7,8 @@
         public DateTime CreatedDate { get; set; }
         public string CreatedBy { get; set; }
         public string Content { get; set; }
+        public Guid? RepliedOnCommentId { get; set; }
+        public string? RepliedOnCommentCreatedBy { get; set; }
+        public string? RepliedOnCommentContent { get; set; }
     }
 }
