4dba7b1cd656b07c9faa3b185dc785cded1f8cc0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-16 18:31:32 +0400

    Add message bus message consumer

---
+++ src/Microservices/Comment/CommentMicroservice.Api/MessageBus/Consumers/CommentDeletedCommentConsumer.cs
@@ -0,0 +1,37 @@
+﻿using CommentMicroservice.Api.Database;
+using MassTransit;
+using MessageBus.Messages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentMicroservice.Api.MessageBus.Consumers
+{
+    public class CommentDeletedCommentConsumer : IConsumer<ICommentDeleted>
+    {
+        private readonly ApplicationDbContext databaseContext;
+
+        public CommentDeletedCommentConsumer(ApplicationDbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public async Task Consume(ConsumeContext<ICommentDeleted> context)
+        {
+            var comments = await databaseContext.Comments.Where(x => x.RepliedOnCommentId == context.Message.CommentId).ToListAsync();
+            var suggestedComments = await databaseContext.SuggestedComments
+                .Where(x => x.RepliedOnCommentId == context.Message.CommentId).ToListAsync();
+
+            foreach (var comment in comments)
+            {
+                comment.RepliedOnCommentId = null;
+                comment.RepliedOnCommentContent = null;
+            }
+
+            foreach (var suggestedComment in suggestedComments)
+            {
+                suggestedComment.RepliedOnCommentId = null;
+                suggestedComment.RepliedOnCommentContent = null;
+            }
+
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
