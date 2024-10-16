de8dda4f5d82bcbb1d361b8f4066eeba9fc39f0f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-16 18:29:55 +0400

    Add migration

---
+++ src/Microservices/Comment/CommentMicroservice.Api/Database/Migrations/20241016131810_AddCommentsRepliesColumns.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommentMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCommentsRepliesColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RepliedOnCommentContent",
+                table: "SuggestedComments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RepliedOnCommentCreatedBy",
+                table: "SuggestedComments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RepliedOnCommentId",
+                table: "SuggestedComments",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RepliedOnCommentContent",
+                table: "Comments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RepliedOnCommentCreatedBy",
+                table: "Comments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RepliedOnCommentId",
+                table: "Comments",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RepliedOnCommentContent",
+                table: "SuggestedComments");
+
+            migrationBuilder.DropColumn(
+                name: "RepliedOnCommentCreatedBy",
+                table: "SuggestedComments");
+
+            migrationBuilder.DropColumn(
+                name: "RepliedOnCommentId",
+                table: "SuggestedComments");
+
+            migrationBuilder.DropColumn(
+                name: "RepliedOnCommentContent",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "RepliedOnCommentCreatedBy",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "RepliedOnCommentId",
+                table: "Comments");
+        }
+    }
+}
