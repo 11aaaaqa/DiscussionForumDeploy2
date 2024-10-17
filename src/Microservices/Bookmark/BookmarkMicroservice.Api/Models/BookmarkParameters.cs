8b7e8fd4e807218dc32c1b4d486249a8586ec2a5
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-17 17:14:20 +0400

    Add pagination model

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Models/BookmarkParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace BookmarkMicroservice.Api.Models
+{
+    public class BookmarkParameters
+    {
+        private const int MaxPageSize = 30;
+        public int PageNumber { get; set; } = 1;
+
+        private int pageSize = 10;
+
+        public int PageSize
+        {
+            get => pageSize;
+            set => pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
