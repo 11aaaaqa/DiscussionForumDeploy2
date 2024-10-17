a5e6585c18a50120183d442acd9482dbc37f6dc7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-17 17:14:32 +0400

    Add service

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Services/BookmarkService.cs
@@ -0,0 +1,44 @@
+﻿using BookmarkMicroservice.Api.Database;
+using BookmarkMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookmarkMicroservice.Api.Services
+{
+    public class BookmarkService : IBookmarkService
+    {
+        private readonly ApplicationDbContext context;
+
+        public BookmarkService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<Bookmark>> GetAllBookmarks(BookmarkParameters bookmarkParameters)
+        {
+            var bookmarks = await context.Bookmarks
+                .Skip(bookmarkParameters.PageSize * (bookmarkParameters.PageNumber - 1))
+                .Take(bookmarkParameters.PageSize).ToListAsync();
+            return bookmarks;
+        }
+
+        public async Task<List<Bookmark>> FindBookmarks(string searchingString, BookmarkParameters bookmarkParameters)
+        {
+            var bookmarks = await context.Bookmarks.Where(x => x.DiscussionTitle.ToLower().Contains(searchingString.ToLower()))
+                .Skip(bookmarkParameters.PageSize * (bookmarkParameters.PageNumber - 1))
+                .Take(bookmarkParameters.PageSize).ToListAsync();
+            return bookmarks;
+        }
+
+        public async Task<Bookmark> AddBookmark(Bookmark bookmark)
+        {
+            await context.Bookmarks.AddAsync(bookmark);
+            await context.SaveChangesAsync();
+            return bookmark;
+        }
+
+        public async Task DeleteBookmark(Guid bookmarkId)
+        {
+            context.Bookmarks.Remove(new Bookmark { Id = bookmarkId });
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Services/IBookmarkService.cs
@@ -0,0 +1,12 @@
+﻿using BookmarkMicroservice.Api.Models;
+
+namespace BookmarkMicroservice.Api.Services
+{
+    public interface IBookmarkService
+    {
+        Task<List<Bookmark>> GetAllBookmarks(BookmarkParameters bookmarkParameters);
+        Task<List<Bookmark>> FindBookmarks(string searchingString, BookmarkParameters bookmarkParameters);
+        Task<Bookmark> AddBookmark(Bookmark bookmark);
+        Task DeleteBookmark(Guid bookmarkId);
+    }
+}
