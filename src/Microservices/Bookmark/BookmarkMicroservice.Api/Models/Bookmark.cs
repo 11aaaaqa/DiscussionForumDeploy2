fbed07bf44b134d06849e7b5ce38b926a340c36c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-17 16:52:33 +0400

    Add a bookmark model

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Models/Bookmark.cs
@@ -0,0 +1,10 @@
+﻿namespace BookmarkMicroservice.Api.Models
+{
+    public class Bookmark
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid DiscussionId { get; set; }
+        public string DiscussionTitle { get; set; }
+    }
+}
