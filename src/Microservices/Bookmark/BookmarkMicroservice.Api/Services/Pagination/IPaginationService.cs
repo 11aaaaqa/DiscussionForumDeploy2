e15fbec12e406672663a03b2e62a7cb2e2cba1da
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-17 17:14:45 +0400

    Add pagination service

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Services/Pagination/IPaginationService.cs
@@ -0,0 +1,10 @@
+﻿using BookmarkMicroservice.Api.Models;
+
+namespace BookmarkMicroservice.Api.Services.Pagination
+{
+    public interface IPaginationService
+    {
+        Task<bool> DoesNextAllBookmarksPageExist(BookmarkParameters  bookmarkParameters);
+        Task<bool> DoesNextFindBookmarksPageExist(string searchingString, BookmarkParameters bookmarkParameters);
+    }
+}
---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Services/Pagination/PaginationService.cs
@@ -0,0 +1,34 @@
+﻿using BookmarkMicroservice.Api.Database;
+using BookmarkMicroservice.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookmarkMicroservice.Api.Services.Pagination
+{
+    public class PaginationService : IPaginationService
+    {
+        private readonly ApplicationDbContext context;
+
+        public PaginationService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<bool> DoesNextAllBookmarksPageExist(BookmarkParameters bookmarkParameters)
+        {
+            int totalBookmarksCount = await context.Bookmarks.CountAsync();
+            int totalRequestedBookmarksCount = bookmarkParameters.PageSize * bookmarkParameters.PageNumber;
+            int startedRequestedBookmarksCount = totalRequestedBookmarksCount - bookmarkParameters.PageSize;
+            bool doesExist = (totalBookmarksCount > startedRequestedBookmarksCount);
+            return doesExist;
+        }
+
+        public async Task<bool> DoesNextFindBookmarksPageExist(string searchingString, BookmarkParameters bookmarkParameters)
+        {
+            int totalBookmarksCount = await context.Bookmarks.Where(x => x.DiscussionTitle.ToLower().Contains(searchingString.ToLower()))
+                .CountAsync();
+            int totalRequestedBookmarksCount = bookmarkParameters.PageSize * bookmarkParameters.PageNumber;
+            int startedRequestedBookmarksCount = totalRequestedBookmarksCount - bookmarkParameters.PageSize;
+            bool doesExist = (totalBookmarksCount > startedRequestedBookmarksCount);
+            return doesExist;
+        }
+    }
+}
