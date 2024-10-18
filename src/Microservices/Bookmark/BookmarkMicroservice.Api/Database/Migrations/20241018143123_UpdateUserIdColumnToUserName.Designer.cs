5d9d0ffd8847555b407ac42f55b8e73656e605a3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-18 18:32:25 +0400

    Add migration

--- src/Microservices/Bookmark/BookmarkMicroservice.Api/Database/Migrations/20241018143123_UpdateUserIdColumnToUserName.Designer.cs
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Database/Migrations/20241018143123_UpdateUserIdColumnToUserName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookmarkMicroservice.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BookmarkMicroservice.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241018143123_UpdateUserIdColumnToUserName")]
+    partial class UpdateUserIdColumnToUserName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/Database/Migrations/20241018143123_UpdateUserIdColumnToUserName.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookmarkMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserIdColumnToUserName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Bookmarks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Bookmarks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Bookmarks");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Bookmarks",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
