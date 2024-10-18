0e1449e5217b5bc9f37ef6b7b84e06c64ce8672b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-18 17:52:17 +0400

    Add getting users boomarks method

---
+++ src/Web/Web.MVC/Web.MVC/Controllers/BookmarkController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.MVC.Models.ApiResponses.Bookmark;
+using Web.MVC.Models.ViewModels.Bookmark;
+
+namespace Web.MVC.Controllers
+{
+    public class BookmarkController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public BookmarkController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [Route("users/{userName}/bookmarks")]
+        [HttpGet]
+        public async Task<IActionResult> GetBookmarks(string userName, Guid userId, int pageSize, int pageNumber)
+        {
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(
+                $"http://bookmark-microservice-api:8080/api/Bookmark/GetBookmarksByUserId/{userId}?pageSize={pageSize}&pageNumber={pageNumber}");
+            if (!response.IsSuccessStatusCode) return View("ActionError");
+
+            var bookmarks = await response.Content.ReadFromJsonAsync<List<BookmarkResponseModel>>();
+
+            var doesNextPageExistResponse = await httpClient.GetAsync(
+                $"http://bookmark-microservice-api:8080/api/Bookmark/DoesNextBookmarksByIdPageExist/{userId}?pageNumber={pageNumber + 1}&pageSize={pageSize}");
+            if (!response.IsSuccessStatusCode) return View("ActionError");
+
+            bool doesExist = await doesNextPageExistResponse.Content.ReadFromJsonAsync<bool>();
+
+            return View(new BookmarkViewModel
+            {
+                PageSize = pageSize, PreviousPageNumber = pageNumber - 1, CurrentPageNumber = pageNumber, NextPageNumber = pageNumber + 1,
+                UserName = userName, UserId = userId, Bookmarks = bookmarks, DoesNextPageExist = doesExist
+            });
+        }
+    }
+}
