53b99d03330759ecec6f283ee93eb2277198be57
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-18 17:51:52 +0400

    Add models

---
+++ src/Web/Web.MVC/Web.MVC/Models/ApiResponses/Bookmark/BookmarkResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.MVC.Models.ApiResponses.Bookmark
+{
+    public class BookmarkResponseModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid DiscussionId { get; set; }
+        public string DiscussionTitle { get; set; }
+    }
+}
---
+++ src/Web/Web.MVC/Web.MVC/Models/ViewModels/Bookmark/BookmarkViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Web.MVC.Models.ApiResponses.Bookmark;
+
+namespace Web.MVC.Models.ViewModels.Bookmark
+{
+    public class BookmarkViewModel
+    {
+        public List<BookmarkResponseModel> Bookmarks { get; set; }
+        public string UserName { get; set; }
+        public Guid UserId { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int NextPageNumber { get; set; }
+        public int PreviousPageNumber { get; set; }
+        public bool DoesNextPageExist { get; set; }
+    }
+}
