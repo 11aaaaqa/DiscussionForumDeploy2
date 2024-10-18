37b93d432437a6a7bd32bcc0814ec3ffb0630cb0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-18 15:25:36 +0400

    Add dto

---
+++ src/Microservices/Bookmark/BookmarkMicroservice.Api/DTOs/BookmarkDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookmarkMicroservice.Api.DTOs
+{
+    public class BookmarkDto
+    {
+        public Guid UserId { get; set; }
+        public Guid DiscussionId { get; set; }
+        public string DiscussionTitle { get; set; }
+    }
+}
