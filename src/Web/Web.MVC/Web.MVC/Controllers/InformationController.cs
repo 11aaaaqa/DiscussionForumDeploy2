91dd4d338824d2317a45b5c66d3d642a8bcf1595
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-20 16:57:39 +0400

    Add controller

---
+++ src/Web/Web.MVC/Web.MVC/Controllers/InformationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.MVC.Controllers
+{
+    public class InformationController : Controller
+    {
+        [Route("info/forbidden")]
+        public IActionResult AccessIsForbidden()
+        {
+            return View();
+        }
+    }
+}
