96f0bf675e47aa1961db1e22f27bf52d2efad7b0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-20 14:40:20 +0400

    Add getting sorted bookmarks methods

--- src/Web/Web.MVC/Web.MVC/Controllers/BookmarkController.cs
+++ src/Web/Web.MVC/Web.MVC/Controllers/BookmarkController.cs
@@ -17,9 +17,9 @@ namespace Web.MVC.Controllers
         }
 
         [Authorize]
-        [Route("users/{userName}/bookmarks")]
+        [Route("users/{userName}/bookmarks/oldest")]
         [HttpGet]
-        public async Task<IActionResult> GetBookmarks(string userName, int pageSize, int pageNumber, string? searchingQuery)
+        public async Task<IActionResult> GetBookmarksByAntiquity(string userName, int pageSize, int pageNumber, string? searchingQuery)
         {
             if (User.Identity.Name != userName) return View("ActionError");
 
@@ -28,7 +28,7 @@ namespace Web.MVC.Controllers
             if (searchingQuery is null)
             {
                 var response = await httpClient.GetAsync(
-                    $"http://bookmark-microservice-api:8080/api/Bookmark/GetBookmarksByUserName/{userName}?pageSize={pageSize}&pageNumber={pageNumber}");
+                    $"http://bookmark-microservice-api:8080/api/Bookmark/GetBookmarksByUserNameByAntiquity/{userName}?pageSize={pageSize}&pageNumber={pageNumber}");
                 if (!response.IsSuccessStatusCode) return View("ActionError");
 
                 var bookmarks = await response.Content.ReadFromJsonAsync<List<BookmarkResponseModel>>();
@@ -46,7 +46,7 @@ namespace Web.MVC.Controllers
             }
 
             var findBookmarksResponse = await httpClient.GetAsync(
-                $"http://bookmark-microservice-api:8080/api/Bookmark/FindBookmarks/{userName}?pageSize={pageSize}&pageNumber={pageNumber}&searchingQuery={searchingQuery}");
+                $"http://bookmark-microservice-api:8080/api/Bookmark/FindBookmarksByAntiquity/{userName}?pageSize={pageSize}&pageNumber={pageNumber}&searchingQuery={searchingQuery}");
             if (!findBookmarksResponse.IsSuccessStatusCode) return View("ActionError");
 
             var foundBookmarks = await findBookmarksResponse.Content.ReadFromJsonAsync<List<BookmarkResponseModel>>();
@@ -61,6 +61,66 @@ namespace Web.MVC.Controllers
             {
                 PageSize = pageSize, CurrentPageNumber = pageNumber, DoesNextPageExist = doesNextFindPageExist, NextPageNumber = pageNumber + 1,
                 PreviousPageNumber = pageNumber - 1, UserName = userName, Bookmarks = foundBookmarks, SearchingQuery = searchingQuery
+            });
+        }
+
+        [Authorize]
+        [Route("users/{userName}/bookmarks/latest")]
+        [HttpGet]
+        public async Task<IActionResult> GetBookmarksByNovelty(string userName, int pageSize, int pageNumber, string? searchingQuery)
+        {
+            if (User.Identity.Name != userName) return View("ActionError");
+
+            using HttpClient httpClient = httpClientFactory.CreateClient();
+
+            if (searchingQuery is null)
+            {
+                var response = await httpClient.GetAsync(
+                    $"http://bookmark-microservice-api:8080/api/Bookmark/GetBookmarksByUserNameByNovelty/{userName}?pageSize={pageSize}&pageNumber={pageNumber}");
+                if (!response.IsSuccessStatusCode) return View("ActionError");
+
+                var bookmarks = await response.Content.ReadFromJsonAsync<List<BookmarkResponseModel>>();
+
+                var doesNextPageExistResponse = await httpClient.GetAsync(
+                    $"http://bookmark-microservice-api:8080/api/Bookmark/DoesNextBookmarksByUserNamePageExist/{userName}?pageSize={pageSize}&pageNumber={pageNumber + 1}");
+                if (!doesNextPageExistResponse.IsSuccessStatusCode) return View("ActionError");
+
+                bool doesExist = await doesNextPageExistResponse.Content.ReadFromJsonAsync<bool>();
+                return View(new BookmarkViewModel
+                {
+                    PageSize = pageSize,
+                    DoesNextPageExist = doesExist,
+                    CurrentPageNumber = pageNumber,
+                    NextPageNumber = pageNumber + 1,
+                    PreviousPageNumber = pageNumber - 1,
+                    UserName = userName,
+                    Bookmarks = bookmarks,
+                    SearchingQuery = searchingQuery
+                });
+            }
+
+            var findBookmarksResponse = await httpClient.GetAsync(
+                $"http://bookmark-microservice-api:8080/api/Bookmark/FindBookmarksByNovelty/{userName}?pageSize={pageSize}&pageNumber={pageNumber}&searchingQuery={searchingQuery}");
+            if (!findBookmarksResponse.IsSuccessStatusCode) return View("ActionError");
+
+            var foundBookmarks = await findBookmarksResponse.Content.ReadFromJsonAsync<List<BookmarkResponseModel>>();
+
+            var doesNextFindPageExistResponse = await httpClient.GetAsync(
+                $"http://bookmark-microservice-api:8080/api/Bookmark/DoesNextFindBookmarksPageExist/{userName}?pageSize={pageSize}&pageNumber={pageNumber + 1}&searchingQuery={searchingQuery}");
+            if (!doesNextFindPageExistResponse.IsSuccessStatusCode) return View("ActionError");
+
+            bool doesNextFindPageExist = await doesNextFindPageExistResponse.Content.ReadFromJsonAsync<bool>();
+
+            return View(new BookmarkViewModel
+            {
+                PageSize = pageSize,
+                CurrentPageNumber = pageNumber,
+                DoesNextPageExist = doesNextFindPageExist,
+                NextPageNumber = pageNumber + 1,
+                PreviousPageNumber = pageNumber - 1,
+                UserName = userName,
+                Bookmarks = foundBookmarks,
+                SearchingQuery = searchingQuery
             });
         }
 
