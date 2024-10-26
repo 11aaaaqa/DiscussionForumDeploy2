1a2b93c759b234d5d506e778ee04c5b94711ad92
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-10-26 18:40:21 +0400

    Add web application factory for testing puprposes

---
+++ src/Microservices/Register/tests/RegisterMicroservice.IntegrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RegisterMicroservice.Api.Database;
+using RegisterMicroservice.Api.Models.UserModels;
+using RegisterMicroservice.Api.Services;
+
+namespace RegisterMicroservice.IntegrationTests
+{
+    public class TestingWebAppFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureLogging(x => x.ClearProviders());
+            builder.ConfigureServices(services =>
+            {
+                var descriptor =
+                    services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+                if (descriptor is not null)
+                    services.Remove(descriptor);
+
+                services.AddTransient<ITokenService, TokenService>();
+                services.AddScoped<IEmailSender, EmailSender>();
+                services.AddTransient<IUserService, UserService>();
+
+                services.AddDbContext<ApplicationDbContext>(x =>
+                    x.UseInMemoryDatabase("RegisterMicroserviceInMemoryDb"));
+
+                var sp = services.BuildServiceProvider();
+                using var appContext = sp.GetRequiredService<ApplicationDbContext>();
+                appContext.Database.EnsureCreated();
+
+                //todo: add users
+                appContext.SaveChanges();
+            });
+        }
+    }
+}
