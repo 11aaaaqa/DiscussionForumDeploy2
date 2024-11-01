6c899e03ef64a00b7a75f561ba13580bf6856af9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-01 19:18:13 +0400

    Add script for dropdown list

---
+++ src/Web/Web.MVC/Web.MVC/wwwroot/js/layout/dropdown.js
@@ -0,0 +1,16 @@
+﻿const button = document.getElementById('dropdownProfileButton');
+const dropdownList = document.getElementById('dropdownProfileList');
+
+button.addEventListener('click', function() {
+    if (dropdownList.style.display === 'none' || dropdownList.style.display === '') {
+        dropdownList.style.display = 'block';
+    } else {
+        dropdownList.style.display = 'none';
+    }
+});
+
+window.addEventListener('click', function() {
+    if (!event.target.matches('#dropdownProfileButton')) {
+        dropdownList.style.display = 'none';
+    }
+});
