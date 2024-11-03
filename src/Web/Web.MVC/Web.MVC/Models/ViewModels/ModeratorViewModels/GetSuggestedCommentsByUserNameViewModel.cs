b7823ef91d72cf3453cd5131e9b7193e521a4dc6
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-03 19:18:02 +0400

    Add view models

---
+++ src/Web/Web.MVC/Web.MVC/Models/ViewModels/ModeratorViewModels/GetSuggestedCommentsByUserNameViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Web.MVC.Models.ApiResponses.CommentsResponses;
+
+namespace Web.MVC.Models.ViewModels.ModeratorViewModels
+{
+    public class GetSuggestedCommentsByUserNameViewModel
+    {
+        public List<SuggestedCommentResponse> SuggestedComments { get; set; }
+        public bool DoesNextPageExist { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int NextPageNumber { get; set; }
+        public int PreviousPageNumber { get; set; }
+        public string SuggestedByUserName { get; set; }
+    }
+}
