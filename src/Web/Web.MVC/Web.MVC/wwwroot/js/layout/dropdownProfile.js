99dc4cef77031be91ff47d666d3ea49913e6b486
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-03 13:54:17 +0400

    Update script name

---
+++ src/Web/Web.MVC/Web.MVC/wwwroot/js/layout/dropdownProfile.js
@@ -0,0 +1,15 @@
+﻿const button = document.getElementById('dropdownProfileButton');
+const list = document.getElementById('dropdownProfileList');
+
+button.addEventListener('click', function (event) {
+    event.stopPropagation();
+    if (list.style.display === 'none' || list.style.display === '') {
+        list.style.display = 'block';
+    } else {
+        list.style.display = 'none';
+    }
+});
+
+window.addEventListener('click', function () {
+    list.style.display = 'none';
+});
