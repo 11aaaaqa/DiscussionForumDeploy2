e958e3a7c2f513d47fab63bc965ce68dd4db3204
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-03 15:30:20 +0400

    Add script

---
+++ src/Web/Web.MVC/Web.MVC/wwwroot/js/user/dropdownUser.js
@@ -0,0 +1,24 @@
+﻿function setupDropdown(buttonId, listId) {
+    const button = document.getElementById(buttonId);
+    const list = document.getElementById(listId);
+
+    button.addEventListener('click', function (event) {
+        event.stopPropagation();
+        if (list.style.display === 'none' || list.style.display === '') {
+            list.style.display = 'block';
+        } else {
+            list.style.display = 'none';
+        }
+    });
+}
+
+setupDropdown('dropdownSuggestionsButton', 'dropdownSuggestionsList');
+setupDropdown('dropdownAdminButton', 'dropdownAdminList');
+
+window.addEventListener('click', function () {
+    const dropdownSuggestionsList = document.getElementById('dropdownSuggestionsList');
+    const dropdownAdminList = document.getElementById('dropdownAdminList');
+
+    dropdownAdminList.style.display = 'none';
+    dropdownSuggestionsList.style.display = 'none';
+});
