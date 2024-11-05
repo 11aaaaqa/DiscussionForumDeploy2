6caa8bc99496daa73216c01b22e47201225447df
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-05 17:50:11 +0400

    Increase max title and body lengths

--- src/Web/Web.MVC/Web.MVC/DTOs/Discussion/DiscussionDto.cs
+++ src/Web/Web.MVC/Web.MVC/DTOs/Discussion/DiscussionDto.cs
@@ -7,12 +7,12 @@ namespace Web.MVC.DTOs.Discussion
         public string TopicName { get; set; }
 
         [Required]
-        [StringLength(70, ErrorMessage = "Максимальная длина заголовка - 70 символов")]
+        [StringLength(150, ErrorMessage = "Максимальная длина заголовка - 150 символов")]
         [Display(Name = "Заголовок")]
         public string Title { get; set; }
 
         [Required]
-        [StringLength(1500, ErrorMessage = "Максимальная длина обсуждения - 1500 символов")]
+        [StringLength(2000, ErrorMessage = "Максимальная длина обсуждения - 2000 символов")]
         [Display(Name = "Обсуждение")]
         public string Content { get; set; }
     }
