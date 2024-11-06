6537c5d959928d856f6e88e0bac13ad312cae82a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-06 19:09:46 +0400

    Add script paints rating

---
+++ src/Web/Web.MVC/Web.MVC/wwwroot/js/discussion/RatingColorChanging.js
@@ -0,0 +1,13 @@
+﻿const ratingElements = document.querySelectorAll('.rating-value');
+ratingElements.forEach(elem => {
+    const value = parseInt(elem.dataset.value, 10);
+
+    if (value === 0) {
+        elem.classList.add('gray');
+    }
+    else if (value > 0) {
+        elem.classList.add('green');
+    } else {
+        elem.classList.add('red');
+    }
+});
