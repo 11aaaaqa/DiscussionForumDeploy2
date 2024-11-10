039143364e137fd5d6e3be30cebc2691a9137fd3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-10 17:40:35 +0400

    Add script

---
+++ src/Web/Web.MVC/Web.MVC/wwwroot/js/discussion/ReplyToComment.js
@@ -0,0 +1,13 @@
+﻿const replyToCommentBtn = document.getElementById('open-reply-to-comment');
+const replyToCommentForm = document.getElementById('reply-to-comment-form');
+const replyToCommentInput = document.querySelector('.reply-to-comment-textarea');
+
+replyToCommentBtn.addEventListener('click', function() {
+    replyToCommentBtn.style.display = 'none';
+    replyToCommentForm.style.display = 'flex';
+});
+
+replyToCommentInput.addEventListener('input', function () {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+});
