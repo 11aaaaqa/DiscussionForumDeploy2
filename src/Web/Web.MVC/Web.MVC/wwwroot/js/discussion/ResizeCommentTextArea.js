bf2b124f6155cd35e2cf001a295b6ba4faa3debd
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-10 15:45:57 +0400

    Add script

---
+++ src/Web/Web.MVC/Web.MVC/wwwroot/js/discussion/ResizeCommentTextArea.js
@@ -0,0 +1,6 @@
+﻿const commentInput = document.querySelector('.leave-a-comment-textarea');
+
+commentInput.addEventListener('input', function() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+});
