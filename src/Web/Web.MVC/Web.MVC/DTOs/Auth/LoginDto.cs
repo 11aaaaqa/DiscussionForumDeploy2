159ec27189f5c206775d8620586dda968e41ca85
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-11 18:40:00 +0400

    Add string length restrictions

--- src/Web/Web.MVC/Web.MVC/DTOs/Auth/LoginDto.cs
+++ src/Web/Web.MVC/Web.MVC/DTOs/Auth/LoginDto.cs
@@ -5,11 +5,13 @@ namespace Web.MVC.DTOs.Auth
     public class LoginDto
     {
         [Required(ErrorMessage = "Поле \"Имя пользователя или адрес эл. почты\" обязательно")]
+        [StringLength(80, ErrorMessage = "Максимальное количество символов почты превышено")]
         [Display(Name = "Имя пользователя или адрес эл. почты")]
         public string UserNameOrEmail { get; set; }
 
         [Required(ErrorMessage = "Поле \"Пароль\" обязательно")]
         [DataType(DataType.Password)]
+        [StringLength(40, ErrorMessage = "Пароль должен содержать как минимум 8 символов", MinimumLength = 8)]
         [Display(Name = "Пароль")]
         public string Password { get; set; }
     }
