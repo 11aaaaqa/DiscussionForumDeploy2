2585915d5b79085086e7e70fb70c06475ab7876b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-11-12 15:17:28 +0400

    Update dto

--- src/Web/Web.MVC/Web.MVC/DTOs/Discussion/DiscussionDto.cs
+++ src/Web/Web.MVC/Web.MVC/DTOs/Discussion/DiscussionDto.cs
@@ -6,12 +6,12 @@ namespace Web.MVC.DTOs.Discussion
     {
         public string TopicName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле \"Заголовок\" обязательно")]
         [StringLength(150, ErrorMessage = "Максимальная длина заголовка - 150 символов")]
         [Display(Name = "Заголовок")]
         public string Title { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле \"Обсуждение\" обязательно")]
         [StringLength(2000, ErrorMessage = "Максимальная длина обсуждения - 2000 символов")]
         [Display(Name = "Обсуждение")]
         public string Content { get; set; }
