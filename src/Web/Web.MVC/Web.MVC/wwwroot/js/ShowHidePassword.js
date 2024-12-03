4ff89cb6ed2192ee5e880d7057a7d36d4d35aad1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-12-03 13:01:06 +0400

    Add showing hiding password script

---
+++ src/Web/Web.MVC/Web.MVC/wwwroot/js/ShowHidePassword.js
@@ -0,0 +1,14 @@
+﻿function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    const eyeIcon = document.getElementById(`eye-icon-${inputId}`);
+    const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+    input.setAttribute('type', type);
+
+    if (type === 'text') {
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+}
