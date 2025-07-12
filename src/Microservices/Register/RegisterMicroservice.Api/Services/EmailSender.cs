e62d45a66a7f6552e40b847626aaa75015212a2a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-12 14:23:09 +0400

    Add logger

--- src/Microservices/Register/RegisterMicroservice.Api/Services/EmailSender.cs
+++ src/Microservices/Register/RegisterMicroservice.Api/Services/EmailSender.cs
@@ -6,13 +6,16 @@ namespace RegisterMicroservice.Api.Services
     public class EmailSender : IEmailSender
     {
         private readonly IConfiguration config;
+        private readonly ILogger<EmailSender> logger;
 
-        public EmailSender(IConfiguration config)
+        public EmailSender(IConfiguration config, ILogger<EmailSender> logger)
         {
             this.config = config;
+            this.logger = logger;
         }
         public async Task SendEmailAsync(MailboxAddress to, string subject, string content)
         {
+            logger.LogInformation("Sending email message method start working");
             var emailMessage = new MimeMessage();
             emailMessage.From.Add(new MailboxAddress(config["Email:CompanyName"], config["Email:From"]));
             emailMessage.To.Add(to);
@@ -20,9 +23,13 @@ namespace RegisterMicroservice.Api.Services
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = content };
 
             using var client = new SmtpClient();
+            logger.LogInformation("Smtp client has been created");
             await client.ConnectAsync(config["Email:Smtp"], int.Parse(config["Email:Port"]), true);
+            logger.LogInformation("Smtp has been connected");
             await client.AuthenticateAsync(config["Email:UserName"], config["Email:Password"]);
+            logger.LogInformation("Smtp user has been authenticated");
             await client.SendAsync(emailMessage);
+            logger.LogInformation("Message has been successfully sent");
 
             await client.DisconnectAsync(true);
         }
